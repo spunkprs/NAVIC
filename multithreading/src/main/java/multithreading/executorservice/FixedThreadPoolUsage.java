8911e2c591328a0369f3f11da6a5da1156f92ce7
Prateek Raj Singh <prateekraj1988@gmail.com>
2024-01-24 08:57:48 +0530

    Fixed Thread Pool Usage git status

---
+++ multithreading/src/main/java/multithreading/executorservice/FixedThreadPoolUsage.java
@@ -0,0 +1,45 @@
+package multithreading.executorservice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class FixedThreadPoolUsage {
+
+    /*
+    Multiple worker threads will be responsible for running submitted tasks to the executor service
+    Tasks will be executed in concurrent manner && number of concurrent tasks running at any given time
+    is equal to the size that we provide while instantiating the thread pool
+    Important thing to notice here is Worker threads{the one executing the underlying tasks} are getting reused,
+    same worker thread id is getting reused multiple once it performs underlying task
+    * */
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 1; i <= 10; i++) {
+            executorService.execute(new SingleThreadExecutor.Task(i));
+        }
+
+        //Shutting down the executor service
+        executorService.shutdown();
+    }
+
+    static class Task implements Runnable {
+
+        private int taskId;
+
+        public Task(int taskId) {
+            this.taskId = taskId;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Task with id " + this.taskId + " is running and have thread id " + Thread.currentThread().getId());
+            //long sleepDuration = (long)Math.random() * 8;
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
