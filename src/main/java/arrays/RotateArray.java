298df7a1e7a7297bf6c3ec9511c5a50134862ade
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-03-23 18:58:19 +0530

    Rotate array accepted solution in O(n) time && O(1) space

--- src/main/java/arrays/RotateArray.java
+++ src/main/java/arrays/RotateArray.java
@@ -9,40 +9,55 @@ public class RotateArray {
 
     public static void main(String ar[]) {
 
+        //Input
+        //int inputArray[] = {-1,-100,3,99};
+        int inputArray [] = {1,2,3,4,5,6,7};
+
+        RotateArray unit = new RotateArray();
+        unit.rotate(inputArray, 10);
     }
 
     public void rotate(int[] nums, int k) {
         if (k > 0 && nums.length > 1) {
-            int numberOfSteps = Math.abs(nums.length - k);
-            processToRotateArrayBySteps(nums, numberOfSteps);
+            int numberOfSteps = k % nums.length;
+            if (numberOfSteps != 0) {
+                processToRotateArrayBySteps(nums, numberOfSteps);
+            }
+        }
+        System.out.println("Array post right rotation ");
+        printElements(nums);
+    }
+
+    private void printElements(int[] nums) {
+        for (Integer num : nums) {
+            System.out.println(num);
         }
     }
 
+    /*
+    *
+    * Process for doing the rotation in O(n) time && O(1) space can be broken down in to following parts :-
+    * Step 1: Reverse elements from n - k - 1 index to n - 1 index
+    * Step 2: Reverse elements from 0 index to n - k - 2 index
+    * Step 3: Reverse elements from 0 index to n - 1 index
+    * */
+
     private void processToRotateArrayBySteps(int nums[], int numberOfSteps) {
-        int counterMaxValue = nums.length;
-        int count = 1;
-        int temp = 0;
-        boolean tempValueSet = false;
-        int startIndex = nums.length - 1;
+        reverse(nums, nums.length - numberOfSteps, nums.length - 1);
+        reverse(nums, 0, nums.length - numberOfSteps - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
 
-        while (count < counterMaxValue) {
-            int nextIndex = startIndex + numberOfSteps;
+    private void reverse(int nums[], int startIndex, int endIndex) {
+        int i = startIndex;
+        int j = endIndex;
 
-            if (nextIndex > nums.length - 1) {
-                nextIndex -= nums.length;
-            }
-
-            if (!tempValueSet) {
-                temp = nums[nextIndex];
-                nums[nextIndex] = nums[startIndex];
-                tempValueSet = true;
-            } else {
-                int tmp = nums[nextIndex];
-                nums[nextIndex] = temp;
-                temp = tmp;
-            }
-            startIndex = nextIndex;
-            count++;
+        while(i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
