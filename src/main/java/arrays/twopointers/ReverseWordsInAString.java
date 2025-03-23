5b5b9ec12593ab2024aee149b69a78c206ee5c0a
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-03-23 13:09:38 +0530

    Reverse words in a string with no extra space, initial commit

---
+++ src/main/java/arrays/twopointers/ReverseWordsInAString.java
@@ -0,0 +1,26 @@
+package arrays.twopointers;
+
+
+/*
+Given a character array s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by a single space.
+
+Problem shall be solved using constant extra space i.e no usage of extra space is allowed
+* */
+
+
+public class ReverseWordsInAString {
+
+    /*
+    *
+    * Problem would be solved in two steps which are following :-
+    * Step 1 : Reverse the characters of the entire string
+    * Step 2 : Now hence characters of individual words are also reversed, we need to re reverse the characters of the individual words to get desired results
+    * */
+
+    public void reverseWords(char[] s) {
+
+    }
+
+}
