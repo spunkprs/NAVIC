5b693e383bbd5a1351f57a0bcbe8bbe18e22f10e
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-03-29 21:24:02 +0530

    Starting with solution of Shortest Word Distance 3

---
+++ src/main/java/arrays/shortestworddistance/ShortestWordDistanceThree.java
@@ -0,0 +1,24 @@
+package arrays.shortestworddistance;
+
+/*
+*
+Given an array of strings wordsDict and two strings that already exist in the array word1 and word2, return the shortest distance between the occurrence of these two words in the list.
+
+Note that word1 and word2 may be the same. It is guaranteed that they represent two individual words in the list.
+
+
+
+Example 1:
+
+Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "coding"
+Output: 1
+Example 2:
+
+Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "makes"
+Output: 3
+* */
+
+public class ShortestWordDistanceThree {
+
+
+}
