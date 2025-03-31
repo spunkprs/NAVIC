dc61226316e9a4e2f280245b281b2f8d5b794cf6
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-03-31 16:15:29 +0530

    Efficient && clean solution for three sum

--- src/main/java/arrays/twopointers/ThreeSum.java
+++ src/main/java/arrays/twopointers/ThreeSum.java
@@ -12,7 +12,8 @@ public class ThreeSum {
 
     /*
      * Solved the question using two pointer approach
-     * Time complexity against Brute force approach as expected is O(pow(n, 3)) but the efficient solution would be to reduce it to O(pow(n, 2)) && tc for the same is O(pow(n, 2)) only
+     * Time complexity against Brute force approach as expected is O(pow(n, 3)) but the efficient solution would be to reduce it to O(pow(n, 2))
+     * && tc for the same is O(pow(n, 2)) only
      * */
 
     public static void main(String ar[]) {
@@ -24,14 +25,14 @@ public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         //Sorting the provided array first in ascending order
         Arrays.sort(nums);
-        Set<Triplet> tripletSet = new HashSet<>();
-        prepareUniqueTripletsHavingSumEqualToZero(nums, tripletSet);
-        return prepareFinalResult(tripletSet);
+        List<Triplet> triplets = new ArrayList<>();
+        prepareUniqueTripletsHavingSumEqualToZero(nums, triplets);
+        return prepareFinalResult(triplets);
     }
 
-    private List<List<Integer>> prepareFinalResult(Set<Triplet> tripletSet) {
+    private List<List<Integer>> prepareFinalResult(List<Triplet> triplets) {
         List<List<Integer>> result = new ArrayList<>();
-        for (Triplet triplet : tripletSet) {
+        for (Triplet triplet : triplets) {
             List<Integer> listOfTriplets = new ArrayList<>();
             listOfTriplets.add(triplet.numOne);
             listOfTriplets.add(triplet.numTwo);
@@ -41,59 +42,56 @@ public class ThreeSum {
         return result;
     }
 
-    private void prepareUniqueTripletsHavingSumEqualToZero(int nums[], Set<Triplet> tripletSet) {
+    private void prepareUniqueTripletsHavingSumEqualToZero(int nums[], List<Triplet> triplets) {
         for (int i = 0; i < nums.length - 2; i++) {
+            //No point in iterating over the array when the first number in the triplet is > 0
+            if (nums[i] > 0) {
+                break;
+            }
+
+            //To avoid unnecessary duplicate addition against first number in the triplet
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
             int numberToBeSearched = -1 * nums[i];
             int leftIndex = i + 1;
             int rightIndex = nums.length - 1;
-            while (leftIndex < rightIndex) {
-                if (nums[leftIndex] + nums[rightIndex] == numberToBeSearched) {
-                    Triplet triplet = new Triplet(nums[i], nums[leftIndex], nums[rightIndex]);
-                    if (!tripletSet.contains(triplet)) {
-                        tripletSet.add(triplet);
+
+                while (leftIndex < rightIndex) {
+                    if (nums[leftIndex] + nums[rightIndex] == numberToBeSearched) {
+                        Triplet triplet = new Triplet(nums[i], nums[leftIndex], nums[rightIndex]);
+                        triplets.add(triplet);
+                        leftIndex++;
+                        rightIndex--;
+
+                        //To avoid unnecessary duplicate addition against second number in the triplet
+                        while (leftIndex < rightIndex && nums[leftIndex] == nums[leftIndex - 1]) {
+                            leftIndex++;
+                        }
+
+                        //To avoid unnecessary duplicate addition against third number in the triplet
+                        while (leftIndex < rightIndex && nums[rightIndex] == nums[rightIndex + 1]) {
+                            rightIndex--;
+                        }
+                    } else if (nums[leftIndex] + nums[rightIndex] < numberToBeSearched) {
+                        leftIndex++;
+                    } else {
+                        rightIndex--;
                     }
-                    leftIndex++;
-                    rightIndex--;
-                } else if (nums[leftIndex] + nums[rightIndex] < numberToBeSearched) {
-                    leftIndex++;
-                } else {
-                    rightIndex--;
                 }
-            }
         }
     }
-
 
     static class Triplet {
         int numOne;
         int numTwo;
         int numThree;
-        int factor;
-        int countOfNonZeroDigits = 3;
-        int modSum;
 
         public Triplet(int numOne, int numTwo, int numThree) {
             this.numOne = numOne;
             this.numTwo = numTwo;
             this.numThree = numThree;
-            this.factor = ((this.numOne == 0 ? 1 : this.numOne) * (this.numTwo == 0 ? 1 :
-                    this.numTwo) * (this.numThree == 0 ? 1 : this.numThree));
-            this.modSum = Math.abs(this.numOne) + Math.abs(this.numTwo) + Math.abs(this.numThree);
-
-            if (this.numOne == 0 || this.numTwo == 0 || this.numThree == 0) {
-                countOfNonZeroDigits--;
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            Triplet triplet = (Triplet) o;
-            return (this.factor == triplet.factor) && (this.countOfNonZeroDigits == triplet.countOfNonZeroDigits) && (this.modSum == triplet.modSum);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.factor);
         }
     }
 
