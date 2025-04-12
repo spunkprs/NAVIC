94622b979415696573f8711795bd0bb011030d0e
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-12 20:42:07 +0530

    Initial commit for sort colors

---
+++ src/main/java/arrays/twopointers/SortColors.java
@@ -0,0 +1,31 @@
+package arrays.twopointers;
+
+
+/*
+*
+* Given an array, colors, which contains a combination of the following three elements:
+*
+0(representing red)
+1(representing white)
+2(representing blue)
+
+Sort the array in place so that the elements of the same color are adjacent, with the colors in the order of red, white, and blue.
+To improve your problem-solving skills, do not utilize the built-in sort function.
+* */
+
+public class SortColors {
+
+    public static void main(String ar[]) {
+        SortColors unit = new SortColors();
+
+        int colors[] = {2, 1, 0, 0, 0, 1, 1, 2};
+    }
+
+    public int[] sortColors (int[] colors) {
+
+        // Write your code here
+
+        return colors;
+    }
+
+}
