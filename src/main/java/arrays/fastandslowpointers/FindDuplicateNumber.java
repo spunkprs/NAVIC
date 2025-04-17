6ef85713bd2d817c634aa6f96651cc3577fd20af
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-17 18:50:41 +0530

    Initial commit for finding duplicates using fast && slow pointer approach

---
+++ src/main/java/arrays/fastandslowpointers/FindDuplicateNumber.java
@@ -0,0 +1,21 @@
+package arrays.fastandslowpointers;
+
+/*
+*
+* Given an array of positive numbers, nums, such that the values lie in the range
+[1,n] where both 1 && n are inclusive, and that there are n+1 numbers in the array,
+* find and return the duplicate number present in nums. There is only one repeated number in nums,
+* but it may appear more than once in the array
+*
+*
+* NOTE : Needs to be solved in constant space
+* */
+
+
+
+public class FindDuplicateNumber {
+
+    public int findDuplicate(int[] nums) {
+        return 0;
+    }
+}
