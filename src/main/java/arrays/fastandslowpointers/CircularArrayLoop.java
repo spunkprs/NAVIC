7790c1e850cf97ca52482464eee79fc125f4e425
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-18 20:29:08 +0530

    Initial commit for Circular Array Loop

---
+++ src/main/java/arrays/fastandslowpointers/CircularArrayLoop.java
@@ -0,0 +1,46 @@
+package arrays.fastandslowpointers;
+
+/*
+*
+There is a circular list of non-zero integers called nums. Each number in the list tells you how many steps to move forward or backward from your current position:
+
+If nums[i] is positive, move nums[i] steps forward.
+
+If nums[i] is negative, move nums[i] steps backward.
+
+As the list is circular:
+
+Moving forward from the last element takes you back to the first element.
+
+Moving backward from the first element takes you to the last element.
+
+A cycle in this list means:
+
+You keep moving according to the numbers, and you end up repeating a sequence of indexes.
+
+All numbers in the cycle have the same sign (either all positive or all negative).
+
+The cycle length is greater than 1 (it involves at least two indexes).
+
+Return true if such a cycle exists in the list or false otherwise.
+
+Constraints:
+1≤ nums.length ≤ pow(10, 3)
+−5000 ≤ nums[i] ≤ 5000
+nums[i] !=0
+
+
+* * */
+
+public class CircularArrayLoop {
+
+    public static void main(String ar[]) {
+        CircularArrayLoop unit = new CircularArrayLoop();
+
+    }
+
+    public static boolean circularArrayLoop(int[] nums) {
+        return false;
+    }
+
+}
