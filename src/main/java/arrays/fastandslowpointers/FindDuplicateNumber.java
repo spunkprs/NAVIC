d68fb3addd935a5f89ffb4e41d3087d507f50b85
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-18 16:40:39 +0530

    Efficient solution for finding the repeating number

--- src/main/java/arrays/fastandslowpointers/FindDuplicateNumber.java
+++ src/main/java/arrays/fastandslowpointers/FindDuplicateNumber.java
@@ -20,6 +20,36 @@ public class FindDuplicateNumber {
     }
 
     public int findDuplicate(int[] nums) {
-        return 0;
+        if (nums.length > 2) {
+            return processToFindDuplicates(nums);
+        } else {
+            return nums[0];
+        }
+    }
+
+    /*
+    * This method effectively makes use of Floyd's Cycle Detection Algorithm for following parts :-
+    * a.) To detect loop in the array
+    * b.) To check from where the loop/cycle starts
+    *
+    * In the context of this problem repeating number is the node from where loop starts
+    * */
+
+    private int processToFindDuplicates(int[] nums) {
+        int slowPointerNext = nums[nums[0]];
+        int fastPointerNext = nums[slowPointerNext];
+
+        while (slowPointerNext != fastPointerNext) {
+            slowPointerNext = nums[slowPointerNext];
+            fastPointerNext = nums[nums[fastPointerNext]];
+        }
+
+        int slowPointerUpdated = nums[0];
+
+        while(slowPointerUpdated != slowPointerNext) {
+            slowPointerNext = nums[slowPointerNext];
+            slowPointerUpdated = nums[slowPointerUpdated];
+        }
+        return slowPointerUpdated;
     }
 }
