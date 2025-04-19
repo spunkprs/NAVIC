4958f03b1c8854f67a7a2bd875bfa65d8faac62a
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-19 20:12:05 +0530

    Accepted solution for CircularArrayLoop

--- src/main/java/arrays/fastandslowpointers/CircularArrayLoop.java
+++ src/main/java/arrays/fastandslowpointers/CircularArrayLoop.java
@@ -32,15 +32,105 @@ nums[i] !=0
 
 * * */
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CircularArrayLoop {
+
+    private boolean cycleFound = false;
 
     public static void main(String ar[]) {
         CircularArrayLoop unit = new CircularArrayLoop();
-
+        int nums[] = {5,4,-2,-1,3};
+        //int nums[] = {1, 2, -3, 4, -2};
+        System.out.print("Cycle detected in this circular array " + unit.circularArrayLoop(nums));
     }
 
-    public static boolean circularArrayLoop(int[] nums) {
+    public boolean circularArrayLoop(int[] nums) {
+        cycleFound = false;
+        if (nums.length > 1) {
+            processToCheckLoopInCircularArray(nums);
+            return cycleFound;
+        }
         return false;
+    }
+
+    private void processToCheckLoopInCircularArray(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(i)) {
+                set.add(i);
+                if (!cycleFound) {
+                   checkExistenceOfLoop(nums, i,  i, set);
+                }
+            }
+        }
+    }
+
+    private void checkExistenceOfLoop(int nums[], int slowPointerIndex, int fastPointerIndex, Set<Integer> set) {
+
+        int fastPointerIndexNextIntermittent = fetchUpdatedIndex(nums, fastPointerIndex);
+        int fastPointerIndexNext = fetchUpdatedIndex(nums, fastPointerIndexNextIntermittent);
+
+        int slowPointerIndexNext = fetchUpdatedIndex(nums, slowPointerIndex);
+
+        if (slowPointerIndex != slowPointerIndexNext && slowPointerIndexNext != -1) {
+            set.add(slowPointerIndexNext);
+        }
+
+        if ((slowPointerIndex == slowPointerIndexNext) || (fastPointerIndexNextIntermittent == fastPointerIndexNext)) {
+            return;
+        }
+
+        if (fastPointerIndexNext == slowPointerIndexNext) {
+             cycleFound = true;
+        } else {
+            checkExistenceOfLoop(nums, slowPointerIndexNext, fastPointerIndexNext, set);
+        }
+    }
+
+    private int fetchUpdatedIndex(int nums[], int index) {
+        if (index != -1) {
+            int num = nums[index];
+            int stepsToJump = Math.abs(num);
+            int updatedIndex = -1;
+            if (num > 0) {
+                if (stepsToJump % nums.length == 0) {
+                    updatedIndex = index;
+                } else {
+                    int step = stepsToJump % nums.length;
+                    int diffSteps = nums.length - 1 - index;
+                    if (step > diffSteps) {
+                        updatedIndex = step - diffSteps - 1;
+                    } else {
+                        updatedIndex = index + step;
+                    }
+                }
+
+                if (nums[updatedIndex] < 0) {
+                    updatedIndex = -1;
+                }
+
+            } else {
+                if (stepsToJump % nums.length == 0) {
+                    updatedIndex = index;
+                } else {
+                    int step = stepsToJump % nums.length;
+                    if (step > index) {
+                        updatedIndex = nums.length - (step - index);
+                    } else {
+                        updatedIndex = index - step;
+                    }
+                }
+
+                if (nums[updatedIndex] > 0) {
+                    updatedIndex = -1;
+                }
+            }
+            return updatedIndex;
+        } else {
+            return -1;
+        }
     }
 
 }
