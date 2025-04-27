c94b9229065d534cb83a8f6cd21a65b853a63121
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-27 13:24:26 +0530

    Initial commit for LongestSubarrayWithAtMostKFrequency

---
+++ src/main/java/arrays/slidingwindow/LongestSubArrayWithAtMostKFrequency.java
@@ -0,0 +1,32 @@
+package arrays.slidingwindow;
+
+/*
+*
+You are given an integer array nums and an integer k.
+The frequency of an element x is the number of times it occurs in an array.
+An array is called good if the frequency of each element in this array is less than or equal to k.
+Return the length of the longest good subarray of nums.
+A subarray is a contiguous non-empty sequence of elements within an array.
+*
+*
+Constraints:
+a.) 1 <= nums.length <= 105
+b.) 1 <= nums[i] <= 109
+c.) 1 <= k <= nums.length
+*
+*
+* */
+
+
+
+public class LongestSubArrayWithAtMostKFrequency {
+
+    public static void main(String ar[]) {
+        LongestSubArrayWithAtMostKFrequency unit = new LongestSubArrayWithAtMostKFrequency();
+    }
+
+    public int maxSubarrayLength(int[] nums, int k) {
+        return 0;
+    }
+
+}
