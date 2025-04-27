b38acb65ca8fbe9ea15ad6120193f46a2eca9849
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-04-27 16:45:12 +0530

    Accepted solution for LongestSubarrayWithAtMostKFrequency

--- src/main/java/arrays/slidingwindow/LongestSubArrayWithAtMostKFrequency.java
+++ src/main/java/arrays/slidingwindow/LongestSubArrayWithAtMostKFrequency.java
@@ -10,23 +10,59 @@ A subarray is a contiguous non-empty sequence of elements within an array.
 *
 *
 Constraints:
-a.) 1 <= nums.length <= 105
-b.) 1 <= nums[i] <= 109
+a.) 1 <= nums.length <= pow(10,5)
+b.) 1 <= nums[i] <= pow(10,9)
 c.) 1 <= k <= nums.length
 *
 *
 * */
 
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubArrayWithAtMostKFrequency {
 
     public static void main(String ar[]) {
         LongestSubArrayWithAtMostKFrequency unit = new LongestSubArrayWithAtMostKFrequency();
+        int inputArr[] = {1,2,1,2,1,2,1,2};
+        int k = 1;
+        System.out.print("Length of longest subarray for the above i/p is :: " + unit.maxSubarrayLength(inputArr, k));
     }
 
     public int maxSubarrayLength(int[] nums, int k) {
-        return 0;
+        return processToFindMaxSubArrayLength(nums, k);
+    }
+
+    private int processToFindMaxSubArrayLength(int[] nums, int k) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxLength = 0;
+
+        while (rightIndex < nums.length) {
+            if (!map.containsKey(nums[rightIndex])) {
+                map.put(nums[rightIndex], 1);
+                maxLength = updatemaxLength(maxLength, leftIndex, rightIndex);
+                rightIndex++;
+            } else {
+                if (map.get(nums[rightIndex]) + 1 <= k) {
+                    map.put(nums[rightIndex], map.get(nums[rightIndex]) + 1);
+                    maxLength = updatemaxLength(maxLength, leftIndex, rightIndex);
+                    rightIndex++;
+                } else {
+                    while (map.get(nums[rightIndex]) + 1 > k) {
+                        map.put(nums[leftIndex], map.get(nums[leftIndex]) - 1);
+                        leftIndex++;
+                    }
+                }
+            }
+        }
+        return maxLength;
+    }
+
+    private int updatemaxLength(int maxLength, int leftIndex, int rightIndex) {
+        return rightIndex - leftIndex + 1 > maxLength ? rightIndex - leftIndex + 1 : maxLength;
     }
 
 }
