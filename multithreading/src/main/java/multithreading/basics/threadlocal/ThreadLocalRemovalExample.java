6d4a73884dac2bbd7736120231265e5acac0fc07
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-02 18:11:39 +0530

    Pushing usage of remove api in ThreadLocal

--- multithreading/src/main/java/multithreading/basics/threadlocal/ThreadLocalRemovalExample.java
+++ multithreading/src/main/java/multithreading/basics/threadlocal/ThreadLocalRemovalExample.java
@@ -2,22 +2,17 @@ package multithreading.basics.threadlocal;
 
 /*
 *
-* The TheadLocal construct allows us to store data that will be accessible only by a specific thread
-* Using it all the individual threads would be working on the values without any overlap, hence no possibilities of
-* unexpected behaviour that arises in the multithreaded environment
+* All the basic information about ThreadLocal has already been documented in the example ThreadLocalBasicExample, go through it
+* for any kind of revision/reference
 *
-* For the sake of simplicity we can think of ThreadLocal as a map where key is the individual thread && value is the data i.e
-* value of the data on which that particular thread is working
-*
-* In this example we will be getting our hands dirty with some basic example of how ThreadLocal shall be used
-* in the context of multithreaded environment
+* In this example we will be going through how values set inside ThreadLocal can be removed against each thread
 *
 * Following are the links that were referred to :-
     a.) https://www.baeldung.com/java-threadlocal
     b.) https://www.youtube.com/watch?v=a_BoqsnVR2U {Must watch}
 * */
 
-public class ThreadLocalBasicExample {
+public class ThreadLocalRemovalExample {
 
     public static void main(String ar[]) {
         ThreadLocal<String> threadLocal = new ThreadLocal<>();
@@ -25,11 +20,13 @@ public class ThreadLocalBasicExample {
         Thread threadOne = new Thread(() -> {
             threadLocal.set("Thread One");
             try {
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Value fetched from ThreadLocal against Thread1 :: " + threadLocal.get());
+            System.out.println("Value fetched from ThreadLocal against Thread1 before removal :: " + threadLocal.get());
+            threadLocal.remove();
+            System.out.println("Value fetched from ThreadLocal against Thread1 post removal :: " + threadLocal.get());
         });
 
         Thread threadTwo = new Thread(() -> {
