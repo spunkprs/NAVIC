9f2a5243941145d9b42029492e6d8dd782ad2ad9
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-02 18:47:17 +0530

    Setting initial value for ThreadLocal

---
+++ multithreading/src/main/java/multithreading/basics/threadlocal/ThreadLocalWithInitialValueExample.java
@@ -0,0 +1,23 @@
+package multithreading.basics.threadlocal;
+
+
+/*
+*
+* All the basic information about ThreadLocal has already been documented in the example ThreadLocalBasicExample, go through it
+* for any kind of revision/reference
+*
+* In this example we will be going through how initial values can be set inside ThreadLocal that will be same for each thread
+* which is going to use it
+*
+* Following are the links that were referred to :-
+    a.) https://www.baeldung.com/java-threadlocal
+    b.) https://www.youtube.com/watch?v=a_BoqsnVR2U {Must watch}
+* */
+
+public class ThreadLocalWithInitialValueExample {
+
+    public static void main(String ar[]) {
+
+    }
+
+}
