2d4eed487488350f836696831c73e9314751f7a0
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-09 16:39:35 +0530

    Needed refactoring

--- multithreading/src/main/java/multithreading/basics/cme/multithread/CmeDemonstrationInMultiThreadEnvironment.java
+++ multithreading/src/main/java/multithreading/basics/cme/multithread/CmeDemonstrationInMultiThreadEnvironment.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class CMEDemonstrationInMultiThreadEnvironment {
+public class CmeDemonstrationInMultiThreadEnvironment {
 
     public static void main( String args[] ) throws Exception {
 
