54550ad6e6e7fd5f7db644e1de6d10b09e76cf71
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-10 14:09:24 +0530

    Documenting details around concurrent hash map learning

---
+++ multithreading/src/main/java/multithreading/basics/concurrenthashmap/ConcurrentHashMapUsage.java
@@ -0,0 +1,26 @@
+package multithreading.basics.concurrenthashmap;
+
+/*
+*
+This class is just a collection of documents/links/videos that have been reffered to build a solid foundation of ConcurrentHashMap
+If required will be adding more links/documents to it
+*
+*
+*
+Links used as a reference are following :-
+a.) https://www.youtube.com/watch?v=Ejw0GQLI9z8
+b.) https://anmolsehgal.medium.com/concurrenthashmap-internal-working-in-java-b2a1a48c7289 {mentions the internals for JDK prior to 1.8}
+c.) https://www.youtube.com/watch?v=OGXAs9fZYTc&t=131s
+d.) https://jenkov.com/tutorials/java-util-concurrent/concurrentmap.html
+e.) https://anmolsehgal.medium.com/hashtable-vs-hashmap-vs-concurrenthashmap-4aa0ff1eecc4
+f.) https://www.youtube.com/watch?app=desktop&v=OGXAs9fZYTc
+g.) https://stackoverflow.com/questions/698638/why-does-concurrenthashmap-prevent-null-keys-and-values
+h.) https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ConcurrentHashMap.html#keySet--
+* */
+
+public class ConcurrentHashMapUsage {
+
+    public static void main(String ar[]) {
+        System.out.println("Usage of Concurrent Hash Map !!");
+    }
+}
