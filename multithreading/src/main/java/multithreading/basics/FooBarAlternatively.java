e7ec855a420c7fa78122cfc9001a799b41b0f65d
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-12 17:28:24 +0530

    Printing foo bar alternatively using different threads using java minitor pattern

---
+++ multithreading/src/main/java/multithreading/basics/FooBarAlternatively.java
@@ -0,0 +1,83 @@
+package multithreading.basics;
+
+public class FooBarAlternatively {
+
+    public static void main(String ar[]) {
+
+        FooBar fooBar = new FooBar(5);
+
+        Thread t1 = new Thread(() -> {
+            try {
+                fooBar.foo();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                fooBar.bar();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static class FooBar {
+        private int n;
+        private int sharedNumber;
+
+        public FooBar(int n) {
+            this.n = n;
+            this.sharedNumber = 1;
+        }
+
+        public void foo() throws InterruptedException {
+
+            for (int i = 0; i < this.n; i++) {
+                try {
+                    synchronized (this) {
+                        while(this.sharedNumber == 2) {
+                            this.wait();
+                        }
+                        System.out.print("foo");
+                        this.sharedNumber = 2;
+                        this.notifyAll();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void bar() throws InterruptedException {
+
+            for (int i = 0; i < this.n; i++) {
+                try {
+                    synchronized (this) {
+                        while(this.sharedNumber == 1) {
+                            this.wait();
+                        }
+                        System.out.print("bar");
+                        this.sharedNumber = 1;
+                        this.notifyAll();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
