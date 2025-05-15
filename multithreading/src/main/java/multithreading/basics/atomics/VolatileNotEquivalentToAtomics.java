f224670cf6213315a5254cf6b3e25113773be8e5
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-15 13:15:26 +0530

    Adding necessary comments

--- multithreading/src/main/java/multithreading/basics/atomics/VolatileNotEquivalentToAtomics.java
+++ multithreading/src/main/java/multithreading/basics/atomics/VolatileNotEquivalentToAtomics.java
@@ -5,6 +5,22 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/*
+Here we instantiate several threads and have them race to change a shared boolean variable’s won value to true.
+The first one to do so is considered to have won the race
+
+If we run the above program enough times especially on a machine with multiple processes we will observe multiple threads printing the winning statement.
+The widget above may not show the faulty run since the VM executing the code may have a single vCPU. The volatile variable doesn’t prevent multiple threads
+from accessing and then updating the variable won.
+A possible sequence resulting in multiple threads declaring themselves as winners can be:-
+
+a.) Thread A reads the value of won which is false.
+b.) Thread B reads the value of won which is false.
+c.) Thread A changes the value of won to true and the variable is updated in main memory for all threads to see.
+d.) Thread B doesn’t know Thread A had read the value of the variable won before Thread B accessed it.
+Thread B too updates the value of won to true
+* */
+
 public class VolatileNotEquivalentToAtomics {
 
         static volatile boolean won = false;
@@ -39,6 +55,13 @@ public class VolatileNotEquivalentToAtomics {
                 es.shutdown();
             }
         }
+
+        /*
+        Variable won being volatile reading from processor cache && register is not allowed hence all the threads{running on same or different processors}
+        will be reading from writing to main memory but atomicity of operation is not guaranteed.
+        We can clearly see here multiple threads can enter into critical section simultaneously && alteration to won's variable value by one thread is
+        not being communicated to another hence possibilities of multiple threads claiming to win the race would be there
+        * */
 
         static void race() {
             if (!won) {
