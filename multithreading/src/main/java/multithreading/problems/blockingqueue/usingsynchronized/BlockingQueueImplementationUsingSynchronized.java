5ee14aa7dcc6472c393f2ad5a90949e3b22acfb5
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-19 08:45:56 +0530

    Adding producer && consumer threads for BoundedBuffer problem achieved using intrinsic lock

--- multithreading/src/main/java/multithreading/problems/blockingqueue/usingsynchronized/BlockingQueueImplementationUsingSynchronized.java
+++ multithreading/src/main/java/multithreading/problems/blockingqueue/usingsynchronized/BlockingQueueImplementationUsingSynchronized.java
@@ -11,4 +11,32 @@ for the producer threads to produce something that can be consumed by the consum
 
 public class BlockingQueueImplementationUsingSynchronized {
 
+    public static void main(String ar[]) {
+        BlockingQueue<String> blockingQueue = new BlockingQueue<>(3);
+
+        Thread producerThreadOne = new Thread(new ProducerThread<String>("1", 1, 100, blockingQueue));
+        Thread producerThreadTwo = new Thread(new ProducerThread<String>("2", 2, 200, blockingQueue));
+        Thread producerThreadThree = new Thread(new ProducerThread<String>("3", 3, 300, blockingQueue));
+        Thread producerThreadFour = new Thread(new ProducerThread<String>("4", 4, 400, blockingQueue));
+        Thread producerThreadFive = new Thread(new ProducerThread<String>("5", 5, 500, blockingQueue));
+
+        producerThreadOne.start();
+        producerThreadTwo.start();
+        producerThreadThree.start();
+        producerThreadFour.start();
+        producerThreadFive.start();
+
+        Thread consumerThreadOne = new Thread(new ConsumerThread<String>(1, 0, blockingQueue));
+        Thread consumerThreadTwo = new Thread(new ConsumerThread<String>(2, 0, blockingQueue));
+        Thread consumerThreadThree = new Thread(new ConsumerThread<String>(3, 0, blockingQueue));
+        Thread consumerThreadFour = new Thread(new ConsumerThread<String>(4, 100, blockingQueue));
+        Thread consumerThreadFive = new Thread(new ConsumerThread<String>(5, 200, blockingQueue));
+
+        consumerThreadOne.start();
+        consumerThreadTwo.start();
+        consumerThreadThree.start();
+        consumerThreadFour.start();
+        consumerThreadFive.start();
+    }
+
 }
---
+++ multithreading/src/main/java/multithreading/problems/blockingqueue/usingsynchronized/ProducerThread.java
@@ -0,0 +1,41 @@
+package multithreading.problems.blockingqueue.usingsynchronized;
+
+public class ProducerThread<T> implements Runnable {
+
+    private T item;
+    private int number;
+    private int sleepTimeInMillis;
+    private BlockingQueue<T> blockingQueue;
+
+    public ProducerThread(T item, int number, int sleepTimeInMillis, BlockingQueue<T> blockingQueue) {
+        this.item = item;
+        this.number = number;
+        this.sleepTimeInMillis = sleepTimeInMillis;
+        this.blockingQueue = blockingQueue;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(sleepTimeInMillis);
+            System.out.println(getDescription() + " " + Thread.currentThread() + " with id " + getNumber() + " pushing item " + getItem() + " in the queue " + blockingQueue.enqueue(item));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getDescription() {
+        return "Producer";
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public T getItem() {
+        return item;
+    }
+}
