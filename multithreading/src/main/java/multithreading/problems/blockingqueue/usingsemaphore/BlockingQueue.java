a2dfedf3d3bc814f232558de204d5a4f9edadb8d
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-19 14:11:48 +0530

    Adding detailed explanantion for BlockingQueue that makes use of Semaphore

--- multithreading/src/main/java/multithreading/problems/blockingqueue/usingsemaphore/BlockingQueue.java
+++ multithreading/src/main/java/multithreading/problems/blockingqueue/usingsemaphore/BlockingQueue.java
@@ -9,6 +9,23 @@ b.) Have made use of singly linked list as an internal data structure {inserts a
 c.) Operations on BlockingQueue are mutually exclusive
 d.) Only two operations {push && pop} are allowed
 e.) Mutual exclusion is achieved using Binary Semaphore that will work just like intrinsic && explicit lock as per functionality is concerned
+f.) Have made use of two semaphores produceSemaphore && consumeSemaphore, their names are self explanatory
+
+Important points to be noticed here are following :-
+
+0.) Calls like acquire() && release() defined inside Semaphore class are atomic
+
+1.) Number of permits against produceSemaphore is equal to the capacity of BoundedBuffer which also means that at max
+these only produce threads will be able to push items into the queue until any of the item is consumed via consumer, this thing is made sure
+by the permit count which once become zero blocks all the threads attempting to acquire the permit till the time permit > 0
+
+2.) Number of permits against consumeSemaphore is equal to 0, which states that consumer threads will be blocked till it's value is greater than 0
+which is being controlled by the producer thread i.e that dictates something is being pushed into the queue and then incrementing permit by 1 at this
+point of time consumeSemaphore.acquire() call inside consumer thread is non blocking
+
+3.) Binary Semaphore is being used like a intrinsic OR explicit lock allowing only one thread to enter into critical section atomically hence
+guarantee correctness
+
 * */
 
 public class BlockingQueue<T> {
--- multithreading/src/main/java/multithreading/problems/blockingqueue/usingsemaphore/BlockingQueueImplementationUsingSemaphore.java
+++ multithreading/src/main/java/multithreading/problems/blockingqueue/usingsemaphore/BlockingQueueImplementationUsingSemaphore.java
@@ -6,7 +6,7 @@ Driver class for the simulation of Bounded Buffer problem achieved using monitor
 Important points to notice that are following :-
 a.) Have kept size of buffer smaller than the available producer && consumer threads {though it's not imperative}
 b.) Velocity of consumption i.e consumer threads is less compared to producer, hence producer threads would be waiting individually
-for the consumer threads to consume something before producer can go ahead && produce
+for the consumer threads to consume something before producer can further go ahead && produce
 * */
 
 public class BlockingQueueImplementationUsingSemaphore {
