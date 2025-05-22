04448742fbed83b4e59bc8e6d6bb17fd4ebc1e9d
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-05-22 12:13:00 +0530

    Demonstrating thread interruption

---
+++ multithreading/src/main/java/multithreading/basics/interupptedexception/InterruptingThreadDemonstration.java
@@ -0,0 +1,48 @@
+package multithreading.basics.interupptedexception;
+
+public class InterruptingThreadDemonstration {
+
+        public static void main( String args[] ) throws InterruptedException {
+            ToBeInterruptedThread toBeInterruptedThreadOne = new ToBeInterruptedThread("interruptedOne");
+            ToBeInterruptedThread toBeInterruptedThreadTwo = new ToBeInterruptedThread("interruptedTwo");
+
+            Thread innerThreadOne = new Thread(toBeInterruptedThreadOne);
+            Thread innerThreadTwo = new Thread(toBeInterruptedThreadTwo);
+
+            innerThreadOne.start();
+            innerThreadTwo.start();
+
+            // Interrupt innerThread after waiting for 5 seconds
+            System.out.println("Main thread sleeping at " + +System.currentTimeMillis() / 1000);
+            Thread.sleep(5000);
+
+            innerThreadOne.interrupt();
+            innerThreadTwo.interrupt();
+
+            System.out.println("Main thread exiting at " + +System.currentTimeMillis() / 1000);
+        }
+
+        static class ToBeInterruptedThread implements Runnable {
+
+            private String threadName;
+
+            public ToBeInterruptedThread(String threadName) {
+                this.threadName = threadName;
+            }
+
+            public String getThreadName() {
+                return threadName;
+            }
+
+            public void run() {
+                try {
+                    System.out.println(getThreadName() + " goes to sleep at " + System.currentTimeMillis() / 1000);
+                    Thread.sleep(1000 * 1000);
+                } catch (InterruptedException ie) {
+                    System.out.println(getThreadName() + " interrupted at " + System.currentTimeMillis() / 1000);
+                }
+            }
+        }
+
+
+}
