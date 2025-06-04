a65a6e70002cea623438bf737621edcbc5b690fc
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-06-04 14:42:51 +0530

    Initial round of commit for meeting rooms 2 problem git status

---
+++ src/main/java/arrays/mergeInterval/MeetingRooms2.java
@@ -0,0 +1,29 @@
+package arrays.mergeInterval;
+
+/**
+ We are given an input array of meeting time intervals, intervals, where each interval has a start time and an end time.
+ Your task is to find the minimum number of meeting rooms required to hold these meetings
+
+ Constraints :-
+ a.) 1<= intervals.length <= pow(10,3)
+ b.) 0 ≤ startIndex < endIndex <= pow(10, 6)
+
+
+ Credits --> Educative
+ References -->
+ a.) https://www.educative.io/interview-prep/coding/meeting-rooms-ii
+ b.) https://www.youtube.com/watch?v=FdzJmTCVyJU
+* */
+
+public class MeetingRooms2 {
+
+    public static void main(String ar[]) {
+
+    }
+
+    public static int findSets(int[][] intervals) {
+
+        // Replace this placeholder return statement with your code
+        return -1;
+    }
+}
