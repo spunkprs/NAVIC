c9f44c54d26b1bfa65bc2323d8bd256468487e19
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-06-05 18:31:52 +0530

    Initial commit for count days without meetings git status

---
+++ src/main/java/arrays/mergeInterval/CountDaysWithoutMeetings.java
@@ -0,0 +1,25 @@
+package arrays.mergeInterval;
+
+/**
+ You are given a positive integer, days, which represents the total number of days an employee is available for work, starting from day 1,
+ You are also given a 2D array, meetings, where each entry meetings[i] = [starti, endi] indicates that a meeting is scheduled from day
+ starti to endi(both inclusive).
+
+ Your task is to count the days when the employee is available for work but has no scheduled meetings
+
+ Credits --> Educative
+ References --> https://www.educative.io/interview-prep/coding/count-days-without-meetings
+*/
+
+public class CountDaysWithoutMeetings {
+
+    public static void main(String ar[]) {
+        CountDaysWithoutMeetings unit = new CountDaysWithoutMeetings();
+    }
+
+    public int countDays(int days, int[][] meetings) {
+
+        // Replace this placeholder return statement with your code
+        return -1;
+    }
+}
