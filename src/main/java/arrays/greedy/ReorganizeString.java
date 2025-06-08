9148482e5493590d95b8a320f24760ca9a1b28e4
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-06-08 17:41:17 +0530

    Initial commit for ReorganozeString problem git status

---
+++ src/main/java/arrays/greedy/ReorganizeString.java
@@ -0,0 +1,32 @@
+package arrays.greedy;
+
+/**
+ Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+ Return any possible rearrangement of s or return "" if not possible.
+
+ Example 1:
+ Input: s = "aab"
+ Output: "aba"
+
+ Example 2:
+ Input: s = "aaab"
+ Output: ""
+
+ Example 3:
+ Input: s = "abbccbb"
+ Output: "bcbcbab"
+
+ Constraints:-
+
+ a.) 1 <= s.length <= 500
+ b.) s consists of lowercase English letters.
+
+ * */
+
+public class ReorganizeString {
+
+    public static void main(String ar[]) {
+
+    }
+}
