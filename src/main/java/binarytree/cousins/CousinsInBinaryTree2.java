a785ffc655f20c809b208fa7327b677b80ff0291
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-06-10 16:37:54 +0530

    Progressive changes for problem CousinsInBinaryTree2 git status

--- src/main/java/binarytree/cousins/CousinsInBinaryTree2.java
+++ src/main/java/binarytree/cousins/CousinsInBinaryTree2.java
@@ -1,5 +1,10 @@
 package binarytree.cousins;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  Given the root of a binary tree, replace the value of each node in the tree with the sum of all its cousins' values.
 
@@ -14,14 +19,51 @@ package binarytree.cousins;
 
 public class CousinsInBinaryTree2 {
 
+    public static void main(String ar[]) {
+        CousinsInBinaryTree2 unit = new CousinsInBinaryTree2();
+
+        TreeNode root = new TreeNode(5);
+
+        System.out.println("Tree post modification according to cousins rules is " + unit.replaceValueInTree(root));
+    }
+
     public TreeNode replaceValueInTree(TreeNode root) {
-        return null;
+        Map<Integer, Long> levelSumMap = new HashMap<>();
+
+        processToUpdateLevelSumMap(root, levelSumMap, 0);
+
+        updateStructureOfTree(root, levelSumMap);
+        return root;
+    }
+
+    private void updateStructureOfTree(TreeNode root, Map<Integer, Long> levelSumMap) {
+        Queue<Node> queue = new LinkedList<>();
+
+    }
+
+    private void processToUpdateLevelSumMap(TreeNode node, Map<Integer, Long> levelSumMap, int level) {
+        if (!levelSumMap.containsKey(level)) {
+            levelSumMap.put(level, new Long(node.val));
+        } else {
+            levelSumMap.put(level, levelSumMap.get(level) + node.val);
+        }
+
+        TreeNode leftNode = node.left;
+        TreeNode rightNode = node.right;
+
+        if (leftNode != null) {
+            processToUpdateLevelSumMap(leftNode, levelSumMap, level + 1);
+        }
+
+        if (rightNode != null) {
+            processToUpdateLevelSumMap(rightNode, levelSumMap, level + 1);
+        }
     }
 
     static class TreeNode {
         private  int val;
-        private CousinsInBinaryTree.TreeNode left;
-        private CousinsInBinaryTree.TreeNode right;
+        private TreeNode left;
+        private TreeNode right;
 
         public TreeNode(int val) {
             this.val = val;
@@ -30,10 +72,10 @@ public class CousinsInBinaryTree2 {
 
 
     static class Node {
-        int initialValue;
-        boolean isLeftChild;
-        int level;
-        TreeNode treeNode;
+        private int initialValue;
+        private boolean isLeftChild;
+        private int level;
+        private TreeNode treeNode;
 
         public Node(int initialValue, boolean isLeftChild, int level, TreeNode treeNode) {
             this.initialValue = initialValue;
