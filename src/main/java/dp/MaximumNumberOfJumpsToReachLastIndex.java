d803f378a77d2875cd0797ee0aef8e91ae2b2a7a
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-07-22 14:51:14 +0530

    Intermittent commit for maximum number of steps to reach last index git status

--- src/main/java/dp/MaximumNumberOfJumpsToReachLastIndex.java
+++ src/main/java/dp/MaximumNumberOfJumpsToReachLastIndex.java
@@ -1,5 +1,10 @@
 package dp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  You are given a 0-indexed array nums of n integers and an integer target.
 
@@ -25,11 +30,56 @@ public class MaximumNumberOfJumpsToReachLastIndex {
 
     public static void main(String ar[]) {
         MaximumNumberOfJumpsToReachLastIndex unit = new MaximumNumberOfJumpsToReachLastIndex();
+        //int nums[] = {1,3,6,4,1,1};
+        //int target = 0;
 
+        int nums[] = {0, 1};
+        int target = 1;
 
+        System.out.println("Maximum number of steps to reach destination from start index 0 with constraints is " + unit.maximumJumps(nums, target));
     }
 
     public int maximumJumps(int[] nums, int target) {
-        return -1;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(nums.length - 1, 0);
+
+        for (int startIndex = nums.length - 1; startIndex >= 1; startIndex--) {
+            processToBuildMaxPathMap(startIndex, nums, map, target);
+        }
+
+        int maxDistanceFromParent = -1;
+
+        for (int startIndex = 1; startIndex < nums.length - 1; startIndex++) {
+            if (Math.abs(nums[startIndex] - nums[0]) <= target && map.containsKey(startIndex)) {
+                int maxDistanceFromChild = map.get(startIndex);
+                if (maxDistanceFromChild != -1 && startIndex != nums.length - 1) {
+                    maxDistanceFromParent = maxDistanceFromChild + 1 > maxDistanceFromParent ? maxDistanceFromChild + 1 : maxDistanceFromParent;
+                } else if (maxDistanceFromChild == 0 && startIndex == nums.length - 1) {
+                    maxDistanceFromParent = maxDistanceFromChild + 1 > maxDistanceFromParent ? maxDistanceFromChild + 1 : maxDistanceFromParent;
+                }
+            }
+        }
+        return maxDistanceFromParent;
+    }
+
+    private void processToBuildMaxPathMap(int parentIndex, int nums[], Map<Integer, Integer> map, int target) {
+        int maxDistanceFromParent = -1;
+        if (!map.containsKey(parentIndex)) {
+            for (Integer childIndex : findChildren(parentIndex, nums)) {
+                if (Math.abs(nums[childIndex] - nums[parentIndex]) <= target && map.containsKey(childIndex) && map.get(childIndex) != -1) {
+                    int maxDistanceFromChild = map.get(childIndex);
+                    maxDistanceFromParent = maxDistanceFromChild + 1 > maxDistanceFromParent ? maxDistanceFromChild + 1 : maxDistanceFromParent;
+                }
+            }
+            map.put(parentIndex, maxDistanceFromParent);
+        }
+    }
+
+    private List<Integer> findChildren(int parentIndex, int[] nums) {
+        List<Integer> childIndexes = new ArrayList<>();
+        for (int i = parentIndex + 1; i < nums.length; i++) {
+            childIndexes.add(i);
+        }
+        return childIndexes;
     }
 }
