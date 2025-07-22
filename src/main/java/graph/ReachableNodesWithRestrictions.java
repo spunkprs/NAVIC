a965beddf6fe84f009d54d7fb754741dad506611
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-07-22 18:21:42 +0530

    Initial commit for reachable nodes with restrictions git status

---
+++ src/main/java/graph/ReachableNodesWithRestrictions.java
@@ -0,0 +1,84 @@
+package graph;
+
+import java.util.*;
+
+
+/**
+ There is an undirected tree with n nodes labeled from 0 to n - 1 and n - 1 edges.
+
+ You are given a 2D integer array edges of length n - 1 where edges[i] = [ai, bi] indicates that there is an edge
+ between nodes ai and bi in the tree. You are also given an integer array restricted which represents restricted nodes.
+
+ Return the maximum number of nodes you can reach from node 0 without visiting a restricted node.
+
+ Note that node 0 will not be a restricted node.
+
+
+ Constraints:
+
+ a.) 2 <= n <= 105
+ b.) edges.length == n - 1
+ c.) edges[i].length == 2
+ d.) 0 <= ai, bi < n
+ e.) ai != bi
+ f.) edges represents a valid tree.
+ g.) 1 <= restricted.length < n
+ h.) 1 <= restricted[i] < n
+ i.) All the values of restricted are unique.
+
+ Source : Leetcode
+ * */
+
+public class ReachableNodesWithRestrictions {
+
+    public static void main(String ar[]) {
+        ReachableNodesWithRestrictions unit = new ReachableNodesWithRestrictions();
+
+    }
+
+    public int reachableNodes(int n, int[][] edges, int[] restricted) {
+        Map<Integer, List<Integer>> edgesMap = new HashMap<>();
+        buildTree(edges, edgesMap);
+        processToFindMaxReachableNodes(edgesMap, 0, restricted);
+        return 0;
+    }
+
+    private void processToFindMaxReachableNodes(Map<Integer, List<Integer>> edgesMap, int startIndex, int[] restrictedNodes) {
+        Set<Integer> visitedNodes = new HashSet<>();
+        visitedNodes.add(startIndex);
+
+        process(startIndex, edgesMap, restrictedNodes, visitedNodes);
+    }
+
+    private void process(int startIndex, Map<Integer, List<Integer>> edgesMap,
+                         int[] restrictedNodes, Set<Integer> visitedNodes) {
+
+
+    }
+
+    private void buildTree(int[][] edges, Map<Integer, List<Integer>> edgesMap) {
+        for (int i = 0; i < edges.length; i++) {
+            int fromNode = edges[i][0];
+            int toNode = edges[i][1];
+            buildEdgesMap(fromNode, toNode, edgesMap);
+        }
+    }
+
+    private void buildEdgesMap(int fromNode, int toNode, Map<Integer, List<Integer>> edgesMap) {
+        if (!edgesMap.containsKey(fromNode)) {
+            List<Integer> toNodes = new ArrayList<>();
+            toNodes.add(toNode);
+            edgesMap.put(fromNode, toNodes);
+        } else {
+            edgesMap.get(fromNode).add(toNode);
+        }
+
+        if (!edgesMap.containsKey(toNode)) {
+            List<Integer> fromNodes = new ArrayList<>();
+            fromNodes.add(fromNode);
+            edgesMap.put(toNode, fromNodes);
+        } else {
+            edgesMap.get(toNode).add(fromNode);
+        }
+    }
+}
