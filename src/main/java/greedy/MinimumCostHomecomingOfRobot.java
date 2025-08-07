4330c24e8d776d55a659c2d013f2768b643ca9a8
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-08-07 19:36:10 +0530

    Adding useful comments git status

--- src/main/java/greedy/MinimumCostHomecomingOfRobot.java
+++ src/main/java/greedy/MinimumCostHomecomingOfRobot.java
@@ -2,6 +2,31 @@ package greedy;
 
 import java.util.*;
 
+
+/**
+ There is an m x n grid, where (0, 0) is the top-left cell and (m - 1, n - 1) is the bottom-right cell. You are given an integer array startPos where startPos = [startrow, startcol] indicates that initially, a robot is at the cell (startrow, startcol). You are also given an integer array homePos where homePos = [homerow, homecol] indicates that its home is at the cell (homerow, homecol).
+
+ The robot needs to go to its home. It can move one cell in four directions: left, right, up, or down, and it can not move outside the boundary. Every move incurs some cost. You are further given two 0-indexed integer arrays: rowCosts of length m and colCosts of length n.
+
+ If the robot moves up or down into a cell whose row is r, then this move costs rowCosts[r].
+ If the robot moves left or right into a cell whose column is c, then this move costs colCosts[c].
+ Return the minimum total cost for this robot to return home.
+
+ Constraints:
+
+ 1.) m == rowCosts.length
+ 2.) n == colCosts.length
+ 3.) 1 <= m, n <= pow(10,5)
+ 4.) 0 <= rowCosts[r], colCosts[c] <= pow(10,4)
+ 5.) startPos.length == 2
+ 6.) homePos.length == 2
+ 7.) 0 <= startrow, homerow < m
+ 8.) 0 <= startcol, homecol < n
+
+ Source : LeetCode
+
+ * */
+
 public class MinimumCostHomecomingOfRobot {
 
     public static void main(String ar[]) {
