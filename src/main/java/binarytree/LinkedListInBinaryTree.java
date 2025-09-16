8a72cf3ee2f4c46295b84fe26b442637efa19815
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-09-16 17:18:14 +0530

    Accepted solution on leetcode for the problem git status

--- src/main/java/binarytree/LinkedListInBinaryTree.java
+++ src/main/java/binarytree/LinkedListInBinaryTree.java
@@ -2,47 +2,86 @@ package binarytree;
 
 import list.ListNode;
 
-public class LinkedListInBinaryTree {
-	
-	private TreeNode rootNode;
-	private boolean result;
-	
-	public boolean isSubPath(ListNode head, TreeNode root) {
-		rootNode = root;
-		processToFindIfTreeContainsLinkesList(root, head, false);
-        return result;
-    }
+import java.util.HashSet;
+import java.util.Set;
 
-	private void processToFindIfTreeContainsLinkesList(TreeNode treeNode, ListNode listNode, boolean proceed) {
-		if (treeNode != null && !result) {
-			TreeNode leftNode = treeNode.left;
-			TreeNode rightNode = treeNode.right;
-			if (rootNode == treeNode) {
-				if (treeNode.val == listNode.getVal()) {
-					processToFindIfTreeContainsLinkesList(leftNode, listNode.getNext(), true);
-					processToFindIfTreeContainsLinkesList(rightNode, listNode.getNext(), true);
-				} else {
-					processToFindIfTreeContainsLinkesList(leftNode, listNode, false);
-					processToFindIfTreeContainsLinkesList(rightNode, listNode, false);
-				}
-			} else {
-				if (proceed && listNode == null) {
-					result = true;
-				} else {
-					if (treeNode.val == listNode.getVal()) {
-						processToFindIfTreeContainsLinkesList(leftNode, listNode.getNext(), true);
-						processToFindIfTreeContainsLinkesList(rightNode, listNode.getNext(), true);
-					} else {
-						processToFindIfTreeContainsLinkesList(leftNode, listNode, false);
-						processToFindIfTreeContainsLinkesList(rightNode, listNode, false);
-					}
-				}
-			}
-		} else {
-			if (proceed && listNode == null) {
-				result = true;
-			}
-		}
+public class LinkedListInBinaryTree {
+
+	public static void main(String ar[]) {
+		LinkedListInBinaryTree unit = new LinkedListInBinaryTree();
+
+		TreeNode root = new TreeNode(1);
+		TreeNode nodeOne = new TreeNode(4);
+		TreeNode nodeTwo = new TreeNode(4);
+		TreeNode nodeThree = new TreeNode(2);
+		TreeNode nodeFour = new TreeNode(5);
+		TreeNode nodeFive = new TreeNode(2);
+		TreeNode nodeSix = new TreeNode(8);
+		TreeNode nodeSeven = new TreeNode(6);
+
+
+		root.left = nodeOne;
+		root.right = nodeTwo;
+		nodeOne.left = nodeThree;
+		nodeTwo.left = nodeFour;
+		nodeTwo.right = nodeFive;
+		nodeThree.right = nodeSix;
+		nodeFive.left = nodeSeven;
+
+		ListNode listNodeOne = new ListNode(2);
+		ListNode listNodeTwo = new ListNode(4);
+		ListNode listNodeThree = new ListNode(6);
+
+		listNodeOne.setNext(listNodeTwo);
+		listNodeTwo.setNext(listNodeThree);
+
+		System.out.println("Is linked list present inside binary tree " + unit.isSubPath(listNodeOne, root));
 	}
 
+	public boolean isSubPath(ListNode head, TreeNode root) {
+		String stringFromList = preparePathFromList(head);
+
+		Set<String> resultPaths = new HashSet<>();
+		StringBuilder sb = new StringBuilder();
+
+		preparePathStrings(root, sb, resultPaths);
+		for (String resultString : resultPaths) {
+			if (resultString.contains(stringFromList)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private String preparePathFromList(ListNode node) {
+		StringBuilder sb = new StringBuilder();
+
+		while (node != null) {
+			sb.append(node.getVal());
+			node = node.getNext();
+		}
+		return sb.toString();
+	}
+
+	private void preparePathStrings(TreeNode node, StringBuilder sb, Set<String> resultPaths) {
+		TreeNode leftNode = node.left;
+		TreeNode rightNode = node.right;
+		sb.append(node.val);
+
+		if (leftNode != null) {
+			preparePathStrings(leftNode, sb, resultPaths);
+			String val = String.valueOf(leftNode.val);
+			sb.delete(sb.length() - val.length() , sb.length());
+		}
+
+		if (rightNode != null) {
+			preparePathStrings(rightNode, sb, resultPaths);
+			String val = String.valueOf(rightNode.val);
+			sb.delete(sb.length() - val.length() , sb.length());
+		}
+
+		if (leftNode == null && rightNode == null) {
+			resultPaths.add(sb.toString());
+		}
+	}
 }
