9cdd08b19bf9f7b10fdc97978aa880f384bda6af
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-09-18 20:41:02 +0530

    Adding time && space compexity as part of comments git status

--- src/main/java/binarytree/PathInZigZagLabelledBinaryTree.java
+++ src/main/java/binarytree/PathInZigZagLabelledBinaryTree.java
@@ -2,6 +2,26 @@ package binarytree;
 
 import java.util.*;
 
+/**
+ In an infinite binary tree where every node has two children, the nodes are labelled in row order.
+
+ In the odd numbered rows (ie., the first, third, fifth,...), the labelling is left to right,
+ while in the even numbered rows (second, fourth, sixth,...), the labelling is right to left.
+
+ Given the label of a node in this tree, return the labels in the path from the root of the tree
+ to the node with that label.
+
+ Constraints:
+
+ 1 <= label <= 10^6
+
+ Source : LeetCode
+
+ Time Complexity : O(log n), where n being the label/num
+ Space Complexity : O(log n), where n being the label/num
+
+ * */
+
 public class PathInZigZagLabelledBinaryTree {
 
     public static void main(String ar[]) {
