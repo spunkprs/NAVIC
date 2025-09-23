db6bccd9f2bfde34899687a7bd686b134aef653b
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-09-23 10:13:12 +0530

    Exploring different methods present in TreeMap initial phase git add TreeMapUsageOne.java

---
+++ src/main/java/TreeMapUsageOne.java
@@ -0,0 +1,23 @@
+
+/**
+Intention is to explore following methods present in TreeMap :-
+1.) higherKey(K key)
+2.) lowerKey(K key)
+3.) headMap(K key, boolean inclusive)
+4.) tailMap(K key, boolean inclusive)
+5.) descendingKeySet()
+6.) firstKey()
+7.) lastKey()
+
+Official document:  https://docs.oracle.com/javase/8/docs/api/java/util/TreeMap.html
+
+------> In this example specifically keys will be sorted in the natural or ascending order <------------
+ * */
+
+
+public class TreeMapUsageOne {
+
+    public static void main(String ar[]) {
+
+    }
+}
