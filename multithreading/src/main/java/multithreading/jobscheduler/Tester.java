d25f46c23a0995680d219aa5b0aedbd9392ac63f
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-04 18:48:06 +0530

    Adding relevant comments git add src/main/java/multithreading/jobscheduler/Tester.java

--- multithreading/src/main/java/multithreading/jobscheduler/Tester.java
+++ multithreading/src/main/java/multithreading/jobscheduler/Tester.java
@@ -4,6 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
 
+/**
+tester code for simulating the behaviour of job scheduler
+Reference material : https://akhiilgupta.medium.com/design-a-multi-threaded-task-scheduler-lld-multi-threaded-construct-eb090c5a8727
+ * */
+
 public class Tester {
 
     public static void main(String ar[]) {
