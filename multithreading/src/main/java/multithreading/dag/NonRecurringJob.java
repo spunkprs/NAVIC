cde2a7c7e60bea8ea2e0302564d5f849365c4450
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-05 17:29:35 +0530

    Adding relevant logic for NonRecurringJob git status

--- multithreading/src/main/java/multithreading/dag/NonRecurringJob.java
+++ multithreading/src/main/java/multithreading/dag/NonRecurringJob.java
@@ -1,5 +1,9 @@
 package multithreading.dag;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class NonRecurringJob extends Job {
 
     public NonRecurringJob(int jobId, long runTimestamp) {
@@ -8,7 +12,28 @@ public class NonRecurringJob extends Job {
 
     @Override
     public void runAssociatedTasks() {
-
+        List<Task> dependentTasks = super.getDependentTasks();
+        int attainSize = dependentTasks.size();
+        boolean shallContinue = true;
+        int index = 0;
+        Set<Integer> visitedTaskIds = new HashSet<>();
+        while (index < attainSize && shallContinue) {
+            for (Task dependentTask : dependentTasks) {
+                if (!visitedTaskIds.contains(dependentTask.getTaskId()) &&
+                        dependentTask.getState().equals(TaskState.SUCCESS)) {
+                    visitedTaskIds.add(dependentTask.getTaskId());
+                    index++;
+                } else if (!visitedTaskIds.contains(dependentTask.getTaskId()) &&
+                        dependentTask.getState().equals(TaskState.FAILURE)) {
+                    shallContinue = false;
+                    System.out.println("Job Id " + super.getJobId() + " execution will be stopped because dependent task with id " +
+                            dependentTask.getTaskId() + " has failed !!");
+                }
+            }
+        }
+        if (shallContinue) {
+            System.out.println("Job Id " + super.getJobId() + " has been executed successfully !!");
+        }
     }
 
     @Override
