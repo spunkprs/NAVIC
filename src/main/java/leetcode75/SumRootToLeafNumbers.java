1711d10c89e28f0b531b5968ad6154c3609743ac
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-15 17:32:16 +0530

    Adding time && space complexity git add leetcode75/

--- src/main/java/leetcode75/SumRootToLeafNumbers.java
+++ src/main/java/leetcode75/SumRootToLeafNumbers.java
@@ -2,6 +2,30 @@ package leetcode75;
 
 import binarytree.TreeNode;
 
+/**
+ You are given the root of a binary tree containing digits from 0 to 9 only.
+
+ Each root-to-leaf path in the tree represents a number.
+
+ For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+ Return the total sum of all root-to-leaf numbers. Test cases are generated
+ so that the answer will fit in a 32-bit integer.
+
+ A leaf node is a node with no children.
+
+
+Constraints:-
+1.) The number of nodes in the tree is in the range [1, 1000].
+2.) 0 <= Node.val <= 9
+3.) The depth of the tree will not exceed 10.
+
+Time Complexity = O(n)
+Space Complexity = O(1)
+
+Source : LeetCode
+Level : Medium
+ * */
+
 public class SumRootToLeafNumbers {
 
     private int result = 0;
