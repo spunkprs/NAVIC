ef524bf27152f8cd7b5d9aeb3ca2b0fc3bc6a821
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-16 15:36:24 +0530

    Accepted solution for the problem git add leetcode75/

---
+++ src/main/java/leetcode75/RotateList.java
@@ -0,0 +1,69 @@
+package leetcode75;
+
+public class RotateList {
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null) {
+            return head;
+        } else {
+            int length = findLengthOfList(head);
+
+            if (length == k || k == 0 || length == 1) {
+                return head;
+            } else {
+                int rotationNum = k > length ? length  - (k % length) : length - k;
+
+                if (rotationNum == length) {
+                    return head;
+                }
+
+                ListNode tempOne = head;
+                ListNode prev = null;
+                int indexOne = 1;
+                ListNode tailStart = null;
+
+                while (indexOne <= rotationNum) {
+                    if (indexOne == 1) {
+                        tailStart = tempOne;
+                    }
+                    indexOne++;
+                    if (prev == null) {
+                        prev = tempOne;
+                    } else {
+                        prev = prev.next;
+                    }
+                    tempOne = tempOne.next;
+                }
+
+                ListNode newHead = tempOne;
+
+                while (tempOne.next != null) {
+                    tempOne = tempOne.next;
+                }
+
+                tempOne.next = tailStart;
+                prev.next = null;
+                return newHead;
+            }
+        }
+    }
+
+    private int findLengthOfList(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+
+    static class ListNode {
+        private int val;
+        private ListNode next;
+        public ListNode(int x) {
+            this.val = x;
+        }
+    }
+
+}
