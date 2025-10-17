7292607c4aa9ad02f17ef3ff72abc15202f4f55a
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-17 16:30:34 +0530

    Adding time && space complexity git add leetcode75/

--- src/main/java/leetcode75/CopyListWithRandomPointer.java
+++ src/main/java/leetcode75/CopyListWithRandomPointer.java
@@ -3,6 +3,40 @@ package leetcode75;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A linked list of length n is given such that each node contains an additional random pointer,
+ * which could point to any node in the list, or null.
+ *
+ * Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes,
+ * where each new node has its value set to the value of its corresponding original node.
+ * Both the next and random pointer of the new nodes should point to new nodes in the
+ * copied list such that the pointers in the original list and copied list represent the
+ * same list state. None of the pointers in the new list should point to nodes in the original list.
+ *
+ * For example, if there are two nodes X and Y in the original list, where X.random --> Y,
+ * then for the corresponding two nodes x and y in the copied list, x.random --> y.
+ *
+ * Return the head of the copied linked list.
+ *
+ * The linked list is represented in the input/output as a list of n nodes.
+ * Each node is represented as a pair of [val, random_index] where:
+
+a.) val: an integer representing Node.val
+b.) random_index: the index of the node (range from 0 to n-1) that the random pointer points to,
+or null if it does not point to any node.
+c.) Your code will only be given the head of the original linked list.
+
+Constraints:-
+
+a.) 0 <= n <= 1000
+b.) -pow(10,4) <= Node.val <= pow(10,4)
+c.) Node.random is null or is pointing to some node in the linked list.
+
+
+Time Complexity = O(n) --> Iterating every node in the list
+Space Complexity = O(n) --> Maintaining mapping from oldNode address to newNode address
+ * */
+
 public class CopyListWithRandomPointer {
 
     public Node copyRandomList(Node head) {
