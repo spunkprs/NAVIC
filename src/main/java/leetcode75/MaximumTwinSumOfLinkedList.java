defcd49ab6df615c68c91455dde185e384266073
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-17 15:09:44 +0530

    Adding time && space complexity git add leetcode75/

--- src/main/java/leetcode75/MaximumTwinSumOfLinkedList.java
+++ src/main/java/leetcode75/MaximumTwinSumOfLinkedList.java
@@ -4,6 +4,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+
+/**
+ In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list
+ is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+
+ For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2.
+ These are the only nodes with twins for n = 4.
+ The twin sum is defined as the sum of a node and its twin.
+
+ Given the head of a linked list with even length, return the maximum twin sum of the linked list
+
+Constraints:
+
+a.) The number of nodes in the list is an even integer in the range [2, pow(10,5)].
+b.) 1 <= Node.val <= pow(10,5)
+
+Time Complexity = O(n) --> Iterating every node in the list
+Space Complexity = O(n) --> Making use of both stack && queue that will possess half of nodes each of the list
+ * */
+
 public class MaximumTwinSumOfLinkedList {
 
     public int pairSum(ListNode head) {
