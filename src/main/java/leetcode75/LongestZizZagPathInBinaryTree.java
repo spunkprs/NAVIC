6fc7e49afd30613f3b223029d7c8c49d9dd9340d
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-17 15:45:01 +0530

    Adding comments git add leetcode75/

--- src/main/java/leetcode75/LongestZizZagPathInBinaryTree.java
+++ src/main/java/leetcode75/LongestZizZagPathInBinaryTree.java
@@ -1,5 +1,29 @@
 package leetcode75;
 
+/**
+You are given the root of a binary tree.
+
+A ZigZag path for a binary tree is defined as follow:
+
+Choose any node in the binary tree and a direction (right or left).
+If the current direction is right, move to the right child of the current node;
+otherwise, move to the left child.
+
+Change the direction from right to left or from left to right.
+Repeat the second and third steps until you can't move in the tree.
+Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+
+Return the longest ZigZag path contained in that tree.
+
+
+Constraints:-
+
+a.) The number of nodes in the tree is in the range [1, 5 * pow(10,4)].
+b.) 1 <= Node.val <= 100
+
+
+ * */
+
 public class LongestZizZagPathInBinaryTree {
 
     private int resultSum = 0;
