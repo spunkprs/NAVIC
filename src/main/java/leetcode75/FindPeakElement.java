c8ed78f91896f10016ea1bc3f0d6c7168ffb6d4d
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-24 18:00:43 +0530

    Adding time && space complexity git add leetcode75/

--- src/main/java/leetcode75/FindPeakElement.java
+++ src/main/java/leetcode75/FindPeakElement.java
@@ -1,6 +1,7 @@
 package leetcode75;
 
 /**
+ Problem : 162
  A peak element is an element that is strictly greater than its neighbors.
 
  Given a 0-indexed integer array nums, find a peak element, and return its index.
