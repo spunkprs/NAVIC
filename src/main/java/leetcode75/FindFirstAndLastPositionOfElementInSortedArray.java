6eecead50dfdb64a38ec3758fac2bfe8d78598d6
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-28 16:25:12 +0530

    Accepted solution for the problem git add leetcode75/

---
+++ src/main/java/leetcode75/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,66 @@
+package leetcode75;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+
+    private int minPosition = Integer.MAX_VALUE;
+    private int maxPosition = Integer.MIN_VALUE;
+
+    public static void main(String ar[]) {
+        FindFirstAndLastPositionOfElementInSortedArray unit = new FindFirstAndLastPositionOfElementInSortedArray();
+
+        int nums[] = {1, 4};
+        int target = 4;
+
+        unit.searchRange(nums, target);
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int resultArr[] = {-1, -1};
+        if (nums.length == 0) {
+            return resultArr;
+        } else if (nums.length > 1) {
+            processToLookForSearchRange(nums, target, 0, nums.length - 1);
+            if (minPosition != Integer.MAX_VALUE && maxPosition != Integer.MIN_VALUE) {
+                resultArr[0] = minPosition;
+                resultArr[1] = maxPosition;
+            }
+            return resultArr;
+        } else {
+            if (nums[0] == target) {
+                resultArr[0] = 0;
+                resultArr[1] = 0;
+            }
+            return resultArr;
+        }
+    }
+
+    private void processToLookForSearchRange(int nums[], int target, int startIndex, int endIndex) {
+        int midIndex = (startIndex + endIndex) / 2;
+
+        int value = nums[midIndex];
+
+        if (target > value) {
+            if (endIndex >= midIndex + 1) {
+                processToLookForSearchRange(nums, target, midIndex + 1, endIndex);
+            }
+        } else if (target < value) {
+            if (startIndex <= midIndex - 1) {
+                processToLookForSearchRange(nums, target, startIndex, midIndex - 1);
+            }
+        } else {
+            updateMinAndMaxIndex(midIndex);
+            if (midIndex - 1 >= 0 && nums[midIndex - 1] == target && startIndex != endIndex && midIndex - 1 < minPosition) {
+                processToLookForSearchRange(nums, target, startIndex,midIndex - 1);
+            }
+
+            if (midIndex + 1 < nums.length && nums[midIndex + 1] == target && startIndex != endIndex && midIndex + 1 > maxPosition) {
+                processToLookForSearchRange(nums, target, midIndex + 1, endIndex);
+            }
+        }
+    }
+
+    private void updateMinAndMaxIndex(int midIndex) {
+        minPosition = midIndex < minPosition ? midIndex : minPosition;
+        maxPosition = midIndex > maxPosition ? midIndex : maxPosition;
+    }
+}
