e1bf02b56bafd1edde2121791325276135b09502
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-29 18:10:16 +0530

    Accepted solution for the problem git add leetcode75/

---
+++ src/main/java/leetcode75/LongestSubStringWithAtMostKDifferentCharacters.java
@@ -0,0 +1,62 @@
+package leetcode75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubStringWithAtMostKDifferentCharacters {
+
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+
+        if (k == 0) {
+            return 0;
+        } else {
+            return processToFetchLongestSubString(s, k);
+        }
+    }
+
+    private int processToFetchLongestSubString(String s, int k) {
+        Map<Character, Integer> map = new HashMap();
+        char arr[] = s.toCharArray();
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        int length = Integer.MIN_VALUE;
+
+        while (rightIndex < arr.length) {
+            if (map.size() < k) {
+                length = updateLength(length, leftIndex, rightIndex);
+                if (!map.containsKey(arr[rightIndex])) {
+                    map.put(arr[rightIndex], 1);
+                } else {
+                    map.put(arr[rightIndex], map.get(arr[rightIndex]) + 1);
+                }
+                rightIndex++;
+            } else if (map.size() == k) {
+                if (!map.containsKey(arr[rightIndex])) {
+                    //length = updateLength(length, leftIndex, rightIndex - 1);
+                    while (map.size() == k) {
+                        int count = map.get(arr[leftIndex]);
+                        if (count == 1) {
+                            map.remove(arr[leftIndex]);
+                        } else {
+                            map.put(arr[leftIndex], map.get(arr[leftIndex]) - 1);
+                        }
+                        leftIndex++;
+                    }
+                    map.put(arr[rightIndex], 1);
+                    length = updateLength(length, leftIndex, rightIndex);
+                } else {
+                    map.put(arr[rightIndex], map.get(arr[rightIndex]) + 1);
+                    length = updateLength(length, leftIndex, rightIndex);
+                }
+                rightIndex++;
+            }
+        }
+        return length;
+    }
+
+    private int updateLength(int length, int leftIndex, int rightIndex) {
+        return rightIndex - leftIndex + 1 > length ? rightIndex - leftIndex + 1 : length;
+    }
+}
