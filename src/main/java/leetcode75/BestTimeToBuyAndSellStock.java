85cd69222fe93f9ffc068a2500d1ebf36879ebae
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-30 16:39:13 +0530

    Adding time && space complexity git add leetcode75/

--- src/main/java/leetcode75/BestTimeToBuyAndSellStock.java
+++ src/main/java/leetcode75/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,26 @@
 package leetcode75;
 
+/**
+Problem : 121
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a
+different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction.
+If you cannot achieve any profit, return 0
+
+Constraints:-
+
+a.) 1 <= prices.length <= pow(10,5)
+b.) 0 <= prices[i] <= pow(10,4)
+
+
+Level : Easy
+Time Complexity = O(n)
+Space Complexity = O(1)
+ * */
+
 public class BestTimeToBuyAndSellStock {
 
     public int maxProfit(int[] prices) {
