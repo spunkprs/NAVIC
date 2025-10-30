3e0a10af5e4189e5826b62579b1825066e1fba69
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-10-30 16:42:31 +0530

    Adding time && space complexity git add leetcode75/

--- src/main/java/leetcode75/BestTimeToBuyAndSellStock2.java
+++ src/main/java/leetcode75/BestTimeToBuyAndSellStock2.java
@@ -1,5 +1,28 @@
 package leetcode75;
 
+
+/**
+Problem : 122
+
+You are given an integer array prices where prices[i] is the price of a given
+stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at
+most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day,
+ensuring you never hold more than one share of the stock.
+
+Find and return the maximum profit you can achieve
+
+Constraints:-
+
+a.) 1 <= prices.length <= 3 * pow(10,4)
+b.) 0 <= prices[i] <= pow(10,4)
+
+Level : Medium
+Time Complexity = O(n)
+Space Complexity = O(1)
+ * */
+
 public class BestTimeToBuyAndSellStock2 {
 
     public int maxProfit(int[] prices) {
