ae097656067417234d12c51e08d64c31eb4f3d5c
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-11-07 16:01:46 +0530

    Adding ddescription as part of comments git add leetcode75/JumpGame3.java

--- src/main/java/leetcode75/JumpGame3.java
+++ src/main/java/leetcode75/JumpGame3.java
@@ -2,6 +2,22 @@ package leetcode75;
 
 import java.util.*;
 
+/**
+Problem : 1306
+
+Given an array of non-negative integers arr, you are initially positioned at start index of the array.
+When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+
+Notice that you can not jump outside of the array at any time
+
+
+Constraints:-
+
+a.) 1 <= arr.length <= 5 * pow(10,4)
+b.) 0 <= arr[i] < arr.length
+c.) 0 <= start < arr.length
+ * */
+
 public class JumpGame3 {
 
 
