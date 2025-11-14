c16d1d12c036c3ab7fbba25ffe8738f8ad722466
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-11-14 17:23:37 +0530

    Adding space && time complexity git add leetcode75/KeysAndRooms.java

--- src/main/java/leetcode75/KeysAndRooms.java
+++ src/main/java/leetcode75/KeysAndRooms.java
@@ -5,6 +5,29 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+
+/**
+Problem : 841
+
+There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+
+When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise
+
+Constraints:-
+
+a.) n == rooms.length
+b.) 2 <= n <= 1000
+c.) 0 <= rooms[i].length <= 1000
+d.) 1 <= sum(rooms[i].length) <= 3000
+e.) 0 <= rooms[i][j] < n
+f.) All the values of rooms[i] are unique.
+
+Time Complexity : O(N + E), where N being number of rooms && E being number of keys present inside each room
+Space Complexity : O(N)
+ * */
+
 public class KeysAndRooms {
 
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
