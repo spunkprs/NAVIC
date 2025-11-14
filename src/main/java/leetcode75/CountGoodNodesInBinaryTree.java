de45e0ff1220e5635924684545bd2ef7f7ea7f10
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-11-14 15:47:44 +0530

    ALtered strategy to gain O(N) time complexity git add leetcode75/CountGoodNodesInBinaryTree.java

--- src/main/java/leetcode75/CountGoodNodesInBinaryTree.java
+++ src/main/java/leetcode75/CountGoodNodesInBinaryTree.java
@@ -12,8 +12,8 @@ Constraints:-
 a.) The number of nodes in the binary tree is in the range [1, 10^5].
 b.) Each node's value is between [-10^4, 10^4]
 
-Time Complexity = O(n * log(n))
-Space Complexity = O(n)
+Time Complexity = O(n * log(n)) --> Updated to O(n)
+Space Complexity = O(n) --> O(n) because of implicit stack usage due to recursion usage
 
  * */
 
@@ -31,11 +31,16 @@ public class CountGoodNodesInBinaryTree {
     }
 
     private void processToFetchGoodNoes(TreeNode node) {
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
-        process(node, treeMap);
+        //TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        //process(node, treeMap);
+        process(node, node.val);
     }
 
-    private void process(TreeNode node, TreeMap<Integer, Integer> treeMap) {
+    /**
+     O(Nlog(N)) time complexity based approach
+     * */
+
+    private void processOne(TreeNode node, TreeMap<Integer, Integer> treeMap) {
         Integer higherKey = treeMap.higherKey(node.val);
 
         if (higherKey == null) {
@@ -52,11 +57,11 @@ public class CountGoodNodesInBinaryTree {
         TreeNode rightNode = node.right;
 
         if (leftNode != null) {
-            process(leftNode, treeMap);
+            processOne(leftNode, treeMap);
         }
 
         if (rightNode != null) {
-            process(rightNode, treeMap);
+            processOne(rightNode, treeMap);
         }
 
         if (treeMap.get(node.val) > 1) {
@@ -64,7 +69,27 @@ public class CountGoodNodesInBinaryTree {
         } else {
             treeMap.remove(node.val);
         }
+    }
 
+    /**
+     O(N) time complexity based approach
+     * */
+
+    private void process(TreeNode node, int highestValueTillNow) {
+        if (node.val >= highestValueTillNow) {
+            goodNodesCount++;
+        }
+
+        TreeNode leftNode = node.left;
+        TreeNode rightNode = node.right;
+
+        if (leftNode != null) {
+            process(leftNode, node.val >= highestValueTillNow ? node.val : highestValueTillNow);
+        }
+
+        if (rightNode != null) {
+            process(rightNode, node.val >= highestValueTillNow ? node.val : highestValueTillNow);
+        }
     }
 
     static class TreeNode {
