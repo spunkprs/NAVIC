bbcd0caa8a5e556f1a1c0ae48742a1687d7a0451
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-11-16 19:43:24 +0530

    Adding description for the problem git add leetcode75/PathSum3.java

--- src/main/java/leetcode75/PathSum3.java
+++ src/main/java/leetcode75/PathSum3.java
@@ -4,6 +4,20 @@ import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+Problem : 437
+
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes)
+
+Constraints:-
+
+a.) The number of nodes in the tree is in the range [0, 1000].
+b.) -10^9 <= Node.val <= 10^9
+c.) -1000 <= targetSum <= 1000
+ * */
+
 public class PathSum3 {
 
     public static void main(String ar[]) {
