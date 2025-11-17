179271c37361f67ef1656ccc323116f2e7af32a8
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-11-17 15:21:51 +0530

    Accepted time && space complexity git add leetcode75/NumberOfProvinces.java

--- src/main/java/leetcode75/NumberOfProvinces.java
+++ src/main/java/leetcode75/NumberOfProvinces.java
@@ -5,6 +5,32 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+
+/**
+Problem : 547
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces
+
+
+Constraints:-
+
+a.) 1 <= n <= 200
+b.) n == isConnected.length
+c.) n == isConnected[i].length
+d.) isConnected[i][j] is 1 or 0.
+e.) isConnected[i][i] == 1
+f.) isConnected[i][j] == isConnected[j][i]
+
+Time Complexity = O(N ^ 2)
+Space Complexity = O(N ^ 2)
+ * */
+
 public class NumberOfProvinces {
 
     public static void main(String ar[]) {
