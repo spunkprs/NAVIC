13fcafdefbeb44e379c99abdc9631c57444b45d2
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-12-11 16:10:35 +0530

    Adding description as part of comments git add leetcode75/BestTimeToBuyAndSellStockWithTxnFees.java

--- src/main/java/leetcode75/BestTimeToBuyAndSellStockWithTxnFees.java
+++ src/main/java/leetcode75/BestTimeToBuyAndSellStockWithTxnFees.java
@@ -1,5 +1,27 @@
 package leetcode75;
 
+/**
+Problem : 714
+ You are given an array prices where prices[i] is the price of a given stock on the ith day,
+ and an integer fee representing a transaction fee.
+
+ Find the maximum profit you can achieve. You may complete as many transactions as you like,
+ but you need to pay the transaction fee for each transaction.
+
+ Note:
+
+ You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+ The transaction fee is only charged once for each stock purchase and sale
+
+ Constraints:-
+
+a.) 1 <= prices.length <= 5 * 10^4
+b.) 1 <= prices[i] < 5 * 10^4
+c.) 0 <= fee < 5 * 10^4
+
+
+ * */
+
 public class BestTimeToBuyAndSellStockWithTxnFees {
 
     public int maxProfit(int[] prices, int fee) {
