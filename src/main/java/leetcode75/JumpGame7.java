c70ff88dc1698f2e0a85fbdcb91e25dcb6668e28
Prateek Raj Singh <prateekraj1988@gmail.com>
2025-12-21 16:47:10 +0530

    Adding Bottom-Up Iterative approach to solve the problem git add leetcode75/JumpGame7.java

--- src/main/java/leetcode75/JumpGame7.java
+++ src/main/java/leetcode75/JumpGame7.java
@@ -22,7 +22,8 @@ Problem : 1871
 The solution makes use of top down DP approach hence making it's a recursive solution but for some of the test cases
 it is getting timed out
 
-Trying iterative DP approach OR Bottom Up approach instead
+Tried iterative DP approach OR Bottom Up approach as well but still getting 108/143 test cases passed,
+for remaining getting timed out concerns
 
  * */
 
@@ -34,7 +35,8 @@ public class JumpGame7 {
         //String s = "01101110";
         int minJump = 2;
         int maxJump = 3;
-        System.out.println(unit.canReach(s, minJump, maxJump));
+        //System.out.println(unit.canReach(s, minJump, maxJump));
+        System.out.println(unit.canReachOne(s, minJump, maxJump));
     }
 
     public boolean canReach(String s, int minJump, int maxJump) {
@@ -47,6 +49,39 @@ public class JumpGame7 {
         return map.get(0);
     }
 
+    public boolean canReachOne(String s, int minJump, int maxJump) {
+        return iterativeApproachToFindCanReachFromSrcToDestination(s, minJump, maxJump);
+    }
+
+
+    /**
+    Bottom-Up DP Approach
+     * */
+    private boolean iterativeApproachToFindCanReachFromSrcToDestination(String s, int minJump, int maxJump) {
+        char arr[] = s.toCharArray();
+        boolean map[] = new boolean[arr.length];
+        map[0] = true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == '0') {
+                for (int j = i - maxJump; j <= i - minJump; j++) {
+                    if (j >= 0 && arr[j] == '0') {
+                        if (map[j]) {
+                            map[i] = true;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                map[i] = false;
+            }
+        }
+        return map[arr.length - 1];
+    }
+
+    /**
+     Top-Down DP Approach
+     * */
     private boolean processToFindCanReachFromSourceToDestination(int parentIndex, char[] arr, int minJump, int maxJump,
                                                               Map<Integer, Boolean> map) {
 
