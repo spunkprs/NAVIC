f242bdc5076b7b49f9283e3d57608e08f3a2e076
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-09 19:23:03 +0530

    Adding description as part of comments git add leetcode75/StepByStepDirectionsToBinaryNodeToAnother.java

--- src/main/java/leetcode75/StepByStepDirectionsToBinaryNodeToAnother.java
+++ src/main/java/leetcode75/StepByStepDirectionsToBinaryNodeToAnother.java
@@ -1,5 +1,31 @@
 package leetcode75;
 
+
+/**
+Problem : 2096
+You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n.
+You are also given an integer startValue representing the value of the start node s, and a different integer
+destValue representing the value of the destination node t.
+
+Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path
+ as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+
+'L' means to go from a node to its left child node.
+'R' means to go from a node to its right child node.
+'U' means to go from a node to its parent node.
+Return the step-by-step directions of the shortest path from node s to node t
+
+Constraints:-
+
+a.) The number of nodes in the tree is n.
+b.) 2 <= n <= 10^5
+c.) 1 <= Node.val <= n
+d.) All the values in the tree are unique.
+e.) 1 <= startValue, destValue <= n
+f.) startValue != destValue
+
+ * */
+
 public class StepByStepDirectionsToBinaryNodeToAnother {
 
     private Node headOne;
