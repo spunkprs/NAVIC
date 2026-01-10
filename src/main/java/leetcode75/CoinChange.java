69f12ecc735b1b69cf76b36fcc6277ab7ab0de8d
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-10 18:18:36 +0530

    Adding description as part of comments git add leetcode75/CoinChange.java

--- src/main/java/leetcode75/CoinChange.java
+++ src/main/java/leetcode75/CoinChange.java
@@ -5,6 +5,19 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+Problem : 322
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You may assume that you have an infinite number of each kind of coin
+
+Constraints:-
+
+a.) 1 <= coins.length <= 12
+b.) 1 <= coins[i] <= 2^31 - 1
+c.) 0 <= amount <= 10^4
+ * */
+
 public class CoinChange {
 
     public static void main(String ar[]) {
