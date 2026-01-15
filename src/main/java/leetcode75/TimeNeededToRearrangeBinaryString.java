950143c90909b26396158b5cecd1afae0c3e6d42
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-15 14:20:01 +0530

    Adding constraints as part of comments git add leetcode75/TimeNeededToRearrangeBinaryString.java

--- src/main/java/leetcode75/TimeNeededToRearrangeBinaryString.java
+++ src/main/java/leetcode75/TimeNeededToRearrangeBinaryString.java
@@ -5,6 +5,11 @@ Problem : 2380
 You are given a binary string s. In one second, all occurrences of "01" are simultaneously replaced with "10". This process repeats until no occurrences of "01" exist.
 
 Return the number of seconds needed to complete this process
+
+ Constraints:-
+
+a.) 1 <= s.length <= 1000
+b.) s[i] is either '0' or '1'
  * */
 
 public class TimeNeededToRearrangeBinaryString {
