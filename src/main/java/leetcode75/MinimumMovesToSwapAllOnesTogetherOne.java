d779895119bd51c1ad5856eab99237f4b5dd896b
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-15 13:49:21 +0530

    Accepted solution for the problem git add leetcode75/MinimumMovesToSwapAllOnesTogetherOne.java

---
+++ src/main/java/leetcode75/MinimumMovesToSwapAllOnesTogetherOne.java
@@ -0,0 +1,48 @@
+package leetcode75;
+
+public class MinimumMovesToSwapAllOnesTogetherOne {
+
+    public static void main(String ar[]) {
+        MinimumMovesToSwapAllOnesTogetherOne unit = new MinimumMovesToSwapAllOnesTogetherOne();
+        int data[] = {1,0,1,0,1,0,1,1,1,0,1,0,0,1,1,1,0,0,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,1,0,
+                0,1,0,1,1,0,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,1,1,0,0,1};
+        System.out.print("Minimum moves to swap all ones together is " + unit.minSwaps(data));
+    }
+
+    public int minSwaps(int[] data) {
+        int minSwaps = Integer.MAX_VALUE;
+        int count = fetchTotalNumberOfOnes(data);
+        if (count == 0 || count == data.length || count == 1) {
+            return 0;
+        } else {
+            int leftPointer = 0;
+            int rightPointer = 0;
+            int numberOfOnes = 0;
+            while (rightPointer < data.length) {
+                if (data[rightPointer] == 1) {
+                    numberOfOnes++;
+                }
+                if (rightPointer - leftPointer + 1 == count) {
+                    minSwaps = Math.min(minSwaps, count - numberOfOnes);
+                    if (data[leftPointer] == 1) {
+                        numberOfOnes--;
+                    }
+                    leftPointer++;
+                }
+                rightPointer++;
+            }
+            return minSwaps;
+        }
+    }
+
+    private int fetchTotalNumberOfOnes(int[] data) {
+        int count = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
