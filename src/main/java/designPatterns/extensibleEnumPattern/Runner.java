a2d7992d5e1623b6295112b77fb6632f07ce19e6
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-22 15:44:51 +0530

    Adding meaningful comments git add designPatterns/extensibleEnumPattern/

--- src/main/java/designPatterns/extensibleEnumPattern/Runner.java
+++ src/main/java/designPatterns/extensibleEnumPattern/Runner.java
@@ -1,5 +1,17 @@
 package designPatterns.extensibleEnumPattern;
 
+/**
+ No, one enum cannot extend another in Java. All enums in Java implicitly extend the abstract base class java.lang.Enum.
+ Since Java does not support multiple inheritance for classes, an enum is restricted to this single parent class and cannot extend any other class or enum.
+
+ Reasons for the Restriction:-
+
+ a.) Single Inheritance: Java classes can only have one direct superclass. Enums use their single inheritance slot for java.lang.Enum.
+ b.) Implicitly Final: Enums are implicitly final unless a constant has a class body, which prevents them from being subclassed by other enums or classes.
+ c.) Type Safety: Enums are designed to represent a fixed, closed set of constants known at compile time.
+
+ * */
+
 public class Runner {
 
     public static void main(String ar[]) {
