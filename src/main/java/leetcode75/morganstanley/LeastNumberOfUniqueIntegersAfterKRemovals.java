64359c85168bbc4feae7de7a98c085edfc9cabe3
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-23 17:36:00 +0530

    Adding time && space complexity as part of comments git add leetcode75/morganstanley/LeastNumberOfUniqueIntegersAfterKRemovals.java

--- src/main/java/leetcode75/morganstanley/LeastNumberOfUniqueIntegersAfterKRemovals.java
+++ src/main/java/leetcode75/morganstanley/LeastNumberOfUniqueIntegersAfterKRemovals.java
@@ -4,6 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
+/**
+Problem : 1481
+Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements
+
+Constraints:-
+
+a.) 1 <= arr.length <= 10^5
+b.) 1 <= arr[i] <= 10^9
+c.) 0 <= k <= arr.length
+
+Time Complexity = O(NLog(N)), where N being number of elements in the array
+Space Complexity = O(N)
+
+ * */
+
 public class LeastNumberOfUniqueIntegersAfterKRemovals {
 
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
