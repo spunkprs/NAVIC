ed78cb3ac307eadb9b7b17f335e7a73300257571
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-01-28 16:54:27 +0530

    Adding time && space complexity git add leetcode75/intervals/NonOverlappingIntervals.java

--- src/main/java/leetcode75/intervals/NonOverlappingIntervals.java
+++ src/main/java/leetcode75/intervals/NonOverlappingIntervals.java
@@ -5,6 +5,22 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+Problem : 435
+Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+Note that intervals which only touch at a point are non-overlapping. For example, [1, 2] and [2, 3] are non-overlapping
+
+Constraints:-
+
+a.) 1 <= intervals.length <= 10^5
+b.) intervals[i].length == 2
+c.) -5 * 10^4 <= starti < endi <= 5 * 10^4
+
+Time Complexity = O(NLog(N))
+Space Complexity = O(N)
+ * */
+
 public class NonOverlappingIntervals {
 
     public static void main(String ar[]) {
