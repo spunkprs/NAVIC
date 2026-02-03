9d5c0cf22dc5316afa4faf9865a6115c7be3217a
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-02-03 09:29:54 +0530

    Adding description as part of comments git add leetcode75/MaximumProductSubArray.java

--- src/main/java/leetcode75/MaximumProductSubArray.java
+++ src/main/java/leetcode75/MaximumProductSubArray.java
@@ -1,5 +1,20 @@
 package leetcode75;
 
+/**
+Problem : 152
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Note that the product of an array with a single element is the value of that element
+
+Constraints:-
+
+a.) 1 <= nums.length <= 2 * 10^4
+b.) -10 <= nums[i] <= 10
+c.) The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+ * */
+
 public class MaximumProductSubArray {
 
     public static void main(String ar[]) {
