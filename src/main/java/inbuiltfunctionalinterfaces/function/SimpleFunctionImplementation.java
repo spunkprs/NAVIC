a1be5dc7ec5f1c851ec9065b8110fd44db727f5d
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-02-04 14:01:54 +0530

    Implementing basic function chaining git add inbuiltfunctionalinterfaces/function/SimpleFunctionImplementation.java

---
+++ src/main/java/inbuiltfunctionalinterfaces/function/SimpleFunctionImplementation.java
@@ -0,0 +1,38 @@
+package inbuiltfunctionalinterfaces.function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ Most important thing to notice here is that we can define Function as on line 17 && pass it to map method
+ but to promote functional programming we can directly pass it's implementation as done already in line 20
+
+Implemented basic function chaining too as part of this exercise !!
+ * */
+
+public class SimpleFunctionImplementation {
+
+    public static void main(String ar[]) {
+        List<String> names = Arrays.asList("Adam", "Alexander", "John", "Tom");
+        //Function<String, Integer> filterPredicate = name -> name.length();
+        List<Integer> result = names.stream()
+                //.map(filterPredicate)
+                .map(word -> word.length())
+                //.filter(filterPredicate)
+                .collect(Collectors.toList());
+        for (Integer lengthOfWord : result) {
+            System.out.println(lengthOfWord);
+        }
+
+
+        //Implementing basic chaining
+
+        Function<Integer, Integer> functionSquare = (a) -> a * a;
+        Function<Integer, Integer> functionAdd = (a) -> a + a;
+
+        System.out.println("Forward chaining result " + functionSquare.andThen(functionAdd).apply(3));
+        System.out.println("Reverse chaining result " + functionSquare.compose(functionAdd).apply(3));
+    }
+}
