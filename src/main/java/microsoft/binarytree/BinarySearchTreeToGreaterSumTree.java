9ef8ee04e9a096e0b5e6739f7978b1c3fb779f50
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-02-22 14:15:47 +0530

    Pushing changes for acceptable solution against the problem git add microsoft/binarytree/BinarySearchTreeToGreaterSumTree.java

--- src/main/java/microsoft/binarytree/BinarySearchTreeToGreaterSumTree.java
+++ src/main/java/microsoft/binarytree/BinarySearchTreeToGreaterSumTree.java
@@ -19,15 +19,87 @@ c.) All the values in the tree are unique.
 
 public class BinarySearchTreeToGreaterSumTree {
 
-    public class TreeNode {
+    public static void main(String ar[]) {
+        BinarySearchTreeToGreaterSumTree unit = new BinarySearchTreeToGreaterSumTree();
+        /*TreeNode root = new TreeNode(4);
+
+        TreeNode one = new TreeNode(1);
+        TreeNode two = new TreeNode(6);
+
+        TreeNode three = new TreeNode(0);
+        TreeNode four = new TreeNode(2);
+        TreeNode five = new TreeNode(5);
+        TreeNode six = new TreeNode(7);
+
+        TreeNode seven = new TreeNode(3);
+        TreeNode eight = new TreeNode(8);
+
+        root.left = one;
+        root.right = two;
+
+        one.left = three;
+        one.right = four;
+
+        two.left = five;
+        two.right = six;
+
+        four.right = seven;
+        six.right = eight;*/
+
+        TreeNode root = new TreeNode(3);
+
+        TreeNode one = new TreeNode(2);
+        TreeNode two = new TreeNode(4);
+
+        TreeNode three = new TreeNode(1);
+
+        root.left = one;
+        root.right = two;
+
+        one.left = three;
+
+        TreeNode greaterSumTreeRoot = unit.bstToGst(root);
+        System.out.print(greaterSumTreeRoot);
+    }
+
+    static class TreeNode {
         private int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
     }
 
     public TreeNode bstToGst(TreeNode root) {
-        return null;
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        processToConvertBstToGst(root, 0);
+        return root;
+    }
+
+    private int processToConvertBstToGst(TreeNode node, int sumPassed) {
+        TreeNode rightNode = node.right;
+        TreeNode leftNode = node.left;
+
+        int sumRecorded = 0;
+
+        if (rightNode != null) {
+            sumRecorded += processToConvertBstToGst(rightNode, sumPassed) + node.val;
+            node.val = sumRecorded;
+        } else if (leftNode != null && rightNode == null) {
+            sumRecorded += sumPassed + node.val;
+            node.val = sumRecorded;
+        }
+
+        if (leftNode != null) {
+            sumRecorded = processToConvertBstToGst(leftNode, sumRecorded);
+        }
+
+        if (leftNode == null && rightNode == null) {
+            node.val += sumPassed;
+            sumRecorded = node.val;
+        }
+        return sumRecorded;
     }
 
 }
