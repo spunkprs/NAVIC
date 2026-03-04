9224861ac9ff3824808dd425fb5b230beb23c3ac
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-04 15:09:03 +0530

    Pushing acceptable solution git add microsoft/arrays/TargetSum.java

---
+++ src/main/java/microsoft/arrays/TargetSum.java
@@ -0,0 +1,75 @@
+package microsoft.arrays;
+
+import java.util.*;
+
+public class TargetSum {
+
+    public static void main(String ar[]) {
+        TargetSum unit = new TargetSum();
+        int nums[] = {1,1,1,1,1};
+        int target = 3;
+
+        System.out.println("Number of ways target " + target + " can be achieved is " + unit.findTargetSumWays(nums, target));
+    }
+
+    public int findTargetSumWays(int[] nums, int target) {
+        Map<Pair, Integer> map = new HashMap<>();
+        return processToFindNumberOfWaysToReachTarget(nums, target, 0, -1, map);
+    }
+
+    private int processToFindNumberOfWaysToReachTarget(int[] nums, int target, int sum, int depth, Map<Pair, Integer> map) {
+        Pair parentPair = new Pair(sum, depth);
+        int childIndex = depth + 1;
+        int intermittentResult = 0;
+
+            List<Pair> pairs = new ArrayList<>();
+
+            pairs.add(new Pair(sum + nums[childIndex] * -1, childIndex));
+            pairs.add(new Pair(sum + nums[childIndex], childIndex));
+
+            if (childIndex == nums.length - 1) {
+                for (Pair p : pairs) {
+                    if (p.sum == target) {
+                        map.put(p, 1);
+                        intermittentResult += map.get(p);
+                    } else {
+                        map.put(p, 0);
+                        intermittentResult += map.get(p);
+                    }
+                }
+            } else {
+                for (Pair p : pairs) {
+                    if (!map.containsKey(p)) {
+                        intermittentResult += processToFindNumberOfWaysToReachTarget(nums, target, p.sum, p.depth, map);
+                    } else {
+                        intermittentResult += map.get(p);
+                    }
+                }
+            }
+        map.put(parentPair, intermittentResult);
+        return map.get(parentPair);
+    }
+
+    static class Pair {
+        private int sum;
+        private int depth;
+
+        public Pair(int sum, int depth) {
+            this.sum = sum;
+            this.depth = depth;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return sum == pair.sum && depth == pair.depth;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(sum, depth);
+        }
+    }
+}
