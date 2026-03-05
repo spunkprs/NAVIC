76b761f676f232a610aa56d15468ab0ccebe90b7
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-05 14:53:17 +0530

    Accepted solution for the problem combination sum 2 git add microsoft/arrays/

---
+++ src/main/java/microsoft/arrays/CombinationSum2.java
@@ -0,0 +1,108 @@
+package microsoft.arrays;
+
+import java.util.*;
+
+public class CombinationSum2 {
+
+    public static void main(String ar[]) {
+        CombinationSum2 unit = new CombinationSum2();
+        //int candidates[] = {10,1,2,7,6,1,5};
+        //int target = 8;
+
+        //int candidates[] = {2,5,2,1,2};
+        //int target = 5;
+
+        int candidates[] = {1,1,2,3};
+        int target = 3;
+
+        List<List<Integer>> finalList = unit.combinationSum2(candidates, target);
+        for (int i = 0; i < finalList.size(); i++) {
+            List<Integer> internalList = finalList.get(i);
+            for (Integer num : internalList) {
+                System.out.print(num);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    private List<List<Integer>> resultantList = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        processToPrepareCombinationSum(candidates, target);
+        return resultantList;
+    }
+
+    private void processToPrepareCombinationSum(int candidates[], int target) {
+        Set<Pair> pairSet = new HashSet<>();
+        List<Integer> intermittentResult = new ArrayList<>();
+
+        for (int i = 0; i < candidates.length; i++) {
+            String pathTillNow = String.valueOf(candidates[i]);
+            Pair pair = new Pair(candidates[i], 0, pathTillNow);
+            if (!pairSet.contains(pair) && pair.sum < target) {
+                pairSet.add(pair);
+                intermittentResult.add(candidates[i]);
+                process(i, candidates[i], pairSet, pair.depth, candidates, target, intermittentResult, pathTillNow);
+                intermittentResult.remove(intermittentResult.size() - 1);
+            } else if (!pairSet.contains(pair) && pair.sum == target) {
+                pairSet.add(pair);
+                intermittentResult.add(candidates[i]);
+                resultantList.add(new ArrayList<>(intermittentResult));
+                break;
+            } else if (!pairSet.contains(pair) && pair.sum > target) {
+                break;
+            }
+        }
+    }
+
+    private void process(int parentIndex, int sumAchievedTillNow, Set<Pair> pairSet, int parentDepth, int candidates[], int target,
+                         List<Integer> intermittentResult, String pathTillNow) {
+        for (int i = parentIndex + 1; i < candidates.length; i++) {
+            String intermittentPath = String.valueOf(candidates[i]);
+            Pair pair = new Pair(candidates[i] + sumAchievedTillNow, parentDepth + 1, pathTillNow + intermittentPath);
+
+            if (!pairSet.contains(pair) && pair.sum < target) {
+                pairSet.add(pair);
+                intermittentResult.add(candidates[i]);
+                process(i, pair.sum, pairSet, pair.depth, candidates, target, intermittentResult, pair.path);
+                intermittentResult.remove(intermittentResult.size() - 1);
+            } else if (!pairSet.contains(pair) && pair.sum == target) {
+                pairSet.add(pair);
+                intermittentResult.add(candidates[i]);
+                resultantList.add(new ArrayList<>(intermittentResult));
+                intermittentResult.remove(intermittentResult.size() - 1);
+                break;
+            } else if (!pairSet.contains(pair) && pair.sum > target) {
+                break;
+            }
+        }
+    }
+
+    static class Pair {
+        private int sum;
+        private int depth;
+        private String path;
+
+        public Pair(int sum, int depth, String path) {
+            this.sum = sum;
+            this.depth = depth;
+            this.path = path;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            String path = pair.path;
+            return sum == pair.sum && depth == pair.depth && this.path.equals(path);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(sum, depth, path);
+        }
+    }
+}
---
+++ src/main/java/microsoft/arrays/MaxEventsThatCanBeAttended.java
@@ -0,0 +1,8 @@
+package microsoft.arrays;
+
+public class MaxEventsThatCanBeAttended {
+
+    public static void main(String ar[]) {
+
+    }
+}
