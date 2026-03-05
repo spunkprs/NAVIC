8dae6e04a0bfa0e13c5a637090969ae551c0c32b
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-05 14:56:40 +0530

    Adding necessary information as part of comments git add microsoft/arrays/CombinationSum2.java

--- src/main/java/microsoft/arrays/CombinationSum2.java
+++ src/main/java/microsoft/arrays/CombinationSum2.java
@@ -2,6 +2,21 @@ package microsoft.arrays;
 
 import java.util.*;
 
+/**
+Problem : 40
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations
+
+Constraints:-
+
+a.) 1 <= candidates.length <= 100
+b.) 1 <= candidates[i] <= 50
+c.) 1 <= target <= 30
+ * */
+
 public class CombinationSum2 {
 
     public static void main(String ar[]) {
