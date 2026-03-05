7220b12dca1dcdf7f980fc9ba6a2b41e39ba4da1
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-05 18:24:01 +0530

    Adding necessary information as part of comments git add microsoft/arrays/MaxEarningsFromTaxi.java

--- src/main/java/microsoft/arrays/MaxEarningsFromTaxi.java
+++ src/main/java/microsoft/arrays/MaxEarningsFromTaxi.java
@@ -2,6 +2,28 @@ package microsoft.arrays;
 
 import java.util.*;
 
+/**
+There are n points on a road you are driving your taxi on. The n points on the road are labeled from 1 to n in the direction you are going,
+ and you want to drive from point 1 to point n to make money by picking up passengers. You cannot change the direction of the taxi.
+
+The passengers are represented by a 0-indexed 2D integer array rides, where rides[i] = [starti, endi, tipi] denotes the ith passenger
+ requesting a ride from point starti to point endi who is willing to give a tipi dollar tip.
+
+For each passenger i you pick up, you earn endi - starti + tipi dollars. You may only drive at most one passenger at a time.
+
+Given n and rides, return the maximum number of dollars you can earn by picking up the passengers optimally.
+
+Note: You may drop off a passenger and pick up a different passenger at the same point.
+
+Constraints:-
+
+a.) 1 <= n <= 10^5
+b.) 1 <= rides.length <= 3 * 10^4
+c.) rides[i].length == 3
+d.) 1 <= starti < endi <= n
+e.) 1 <= tipi <= 10^5
+ * */
+
 public class MaxEarningsFromTaxi {
 
     public static void main(String ar[]) {
