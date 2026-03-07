d76fb146c7b0cf259a4bc549ac88f6d97fa3638c
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-07 15:55:26 +0530

    Adding imperative comments along with time && space complexity git add microsoft/binarytree/InvertBinaryTree.java

--- src/main/java/microsoft/binarytree/InvertBinaryTree.java
+++ src/main/java/microsoft/binarytree/InvertBinaryTree.java
@@ -1,5 +1,18 @@
 package microsoft.binarytree;
 
+/**
+Problem : 226
+Given the root of a binary tree, invert the tree, and return its root
+
+Constraints:-
+
+a.) The number of nodes in the tree is in the range [0, 100].
+b.) -100 <= Node.val <= 100
+
+Time Complexity = O(N), where N being number of nodes in the tree
+Space Complexity = O(K), where K being depth of the tree [No additional memory is utilised but it's because of call stack due to recursive approach]
+ * */
+
 public class InvertBinaryTree {
 
     static class TreeNode {
