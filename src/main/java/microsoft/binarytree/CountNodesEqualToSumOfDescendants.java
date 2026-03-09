11f2df0d213f1ae4639e0c9abb3b8fc95a2e68b2
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-09 09:09:17 +0530

    Adding time && space complexity git add microsoft/binarytree/CountNodesEqualToSumOfDescendants.java

--- src/main/java/microsoft/binarytree/CountNodesEqualToSumOfDescendants.java
+++ src/main/java/microsoft/binarytree/CountNodesEqualToSumOfDescendants.java
@@ -1,5 +1,20 @@
 package microsoft.binarytree;
 
+/**
+Problem : 1973
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the sum of the values of its descendants.
+
+A descendant of a node x is any node that is on the path from node x to some leaf node. The sum is considered to be 0 if the node has no descendants.
+
+
+Constraints:-
+a.) The number of nodes in the tree is in the range [1, 10^5].
+b.) 0 <= Node.val <= 10^5
+
+Time Complexity = O(N), where N being number of nodes in the tree
+Space Complexity = O(N), no external space used but space occupied will be because of recursive in memory stack
+ * */
+
 public class CountNodesEqualToSumOfDescendants {
 
     static class TreeNode {
