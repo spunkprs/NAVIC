08f3a48a950e76ea45571d508d596b96b1f957cd
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-09 18:23:28 +0530

    Initial level of commit for the problem git add microsoft/bfs/MinimumKnightMoves.java

---
+++ src/main/java/microsoft/bfs/MinimumKnightMoves.java
@@ -0,0 +1,22 @@
+package microsoft.bfs;
+
+/**
+Problem : 1197
+In an infinite chess board with coordinates from -infinity to +infinity, you have a knight at square [0, 0].
+
+A knight has 8 possible moves it can make, as illustrated below. Each move is two squares in a cardinal direction, then one square in an orthogonal direction.
+
+Constraints:-
+
+a.) -300 <= x, y <= 300
+b.) 0 <= |x| + |y| <= 300
+
+ * */
+
+public class MinimumKnightMoves {
+
+    public int minKnightMoves(int x, int y) {
+        return 0;
+    }
+
+}
