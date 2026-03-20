33879e1508f4eedc5633e5a94af8392a6eb61792
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-20 20:55:10 +0530

    Adding necessary information as part of comments git add agoda/RemoveAllAdjacentDuplicatesInString2.java

--- src/main/java/agoda/RemoveAllAdjacentDuplicatesInString2.java
+++ src/main/java/agoda/RemoveAllAdjacentDuplicatesInString2.java
@@ -3,6 +3,21 @@ package agoda;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them,
+causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+Constraints:-
+
+a.) 1 <= s.length <= 10^5
+b.) 2 <= k <= 10^4
+c.) s only contains lowercase English letters.
+ * */
+
 public class RemoveAllAdjacentDuplicatesInString2 {
 
     public static void main(String ar[]) {
