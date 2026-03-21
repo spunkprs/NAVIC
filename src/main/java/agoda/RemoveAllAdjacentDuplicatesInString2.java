c217214f09f12e1f2249e3fbeb16846cda73758c
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-21 16:07:14 +0530

    Adding imperative comments git add agoda/RemoveAllAdjacentDuplicatesInString2.java

--- src/main/java/agoda/RemoveAllAdjacentDuplicatesInString2.java
+++ src/main/java/agoda/RemoveAllAdjacentDuplicatesInString2.java
@@ -1,6 +1,7 @@
 package agoda;
 
 /**
+Problem : 1209
 You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them,
 causing the left and the right side of the deleted substring to concatenate together.
 
