a81a8ce86b03c82e6206cfebf15ff488d14ec02f
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-21 16:01:42 +0530

    Accepted solution for the problem git add agoda/RemoveAllAdjacentDuplicatesInString2.java

--- src/main/java/agoda/RemoveAllAdjacentDuplicatesInString2.java
+++ src/main/java/agoda/RemoveAllAdjacentDuplicatesInString2.java
@@ -1,8 +1,5 @@
 package agoda;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
 You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them,
 causing the left and the right side of the deleted substring to concatenate together.
@@ -22,85 +19,27 @@ public class RemoveAllAdjacentDuplicatesInString2 {
 
     public static void main(String ar[]) {
         RemoveAllAdjacentDuplicatesInString2 unit = new RemoveAllAdjacentDuplicatesInString2();
-        //String s = "deeedbbcccbdaa";
-        String s = "aaaa";
-        //int k = 3;
-        int k = 2;
+        String s = "deeedbbcccbdaa";
+        //String s = "aaaa";
+        int k = 3;
+        //int k = 2;
         System.out.print(unit.removeDuplicates(s, k));
     }
 
     public String removeDuplicates(String s, int k) {
-
-        char arr[] = s.toCharArray();
-        int i = 0;
-        int windowSize = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        int windowMinIndex = Integer.MAX_VALUE;
-        int windowMaxIndex = Integer.MIN_VALUE;
-
-        while (!s.isEmpty() && i < s.length()) {
-            if (windowSize == k) {
-                if (map.size() == 1) {
-                    if (s.length() == windowSize) {
-                        s = "";
-                    } else {
-                        if (windowMinIndex > 0 && windowMaxIndex < s.length() - 1) {
-                            String preString = s.substring(0, windowMinIndex);
-                            String postString = s.substring(windowMaxIndex + 1, s.length());
-                            s = preString + postString;
-                        } else if (windowMinIndex == 0 && windowMaxIndex < s.length() - 1) {
-                            s = s.substring(windowMaxIndex + 1, s.length());
-                        } else if (windowMinIndex > 0 && windowMaxIndex == s.length() - 1) {
-                            s = s.substring(0, windowMinIndex);
-                        }
-                        arr = s.toCharArray();
-                        i = 0;
-                        windowSize = 0;
-                        windowMinIndex = Integer.MAX_VALUE;
-                        windowMaxIndex = Integer.MIN_VALUE;
-                        map = new HashMap<>();
-                        if (windowSize < k) {
-                            pushElementIntoMap(arr[i], map);
-                            windowSize++;
-                            windowMinIndex = i < windowMinIndex ? i : windowMinIndex;
-                            windowMaxIndex = i > windowMaxIndex ? i : windowMaxIndex;
-                        }
-                    }
-                } else {
-                    removeElementFromMap(arr[windowMinIndex], map);
-                    windowSize--;
-                    windowMinIndex++;
+        StringBuilder sb = new StringBuilder(s);
+        int counter[] = new int[sb.length()];
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                counter[i] = 1;
+            } else if (sb.charAt(i) == sb.charAt(i - 1)) {
+                counter[i] = counter[i - 1] + 1;
+                if (counter[i] == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
                 }
-            } else if (windowSize < k) {
-                pushElementIntoMap(arr[i], map);
-                windowSize++;
-                windowMinIndex = i < windowMinIndex ? i : windowMinIndex;
-                windowMaxIndex = i > windowMaxIndex ? i : windowMaxIndex;
-            }
-
-            if (windowSize < k  && i + 1 < s.length()) {
-                i++;
-            } else if (windowSize < k && i + 1 == s.length()) {
-                break;
             }
         }
-
-        return s;
-    }
-
-    private void removeElementFromMap(char character, Map<Character, Integer> map) {
-        if (map.get(character) > 1) {
-            map.put(character, map.get(character) - 1);
-        } else if (map.get(character) == 1) {
-            map.remove(character);
-        }
-    }
-
-    private void pushElementIntoMap(char character, Map<Character, Integer> map) {
-        if (!map.containsKey(character)) {
-            map.put(character, 1);
-        } else {
-            map.put(character, map.get(character) + 1);
-        }
+        return sb.toString();
     }
 }
