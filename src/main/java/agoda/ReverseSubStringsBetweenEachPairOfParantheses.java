44e69961fed88e879ff424bd6ae936f0d84c3f75
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-22 17:03:07 +0530

    Partially accepted solution for the problem git add agoda/ReverseSubStringsBetweenEachPairOfParantheses.java

---
+++ src/main/java/agoda/ReverseSubStringsBetweenEachPairOfParantheses.java
@@ -0,0 +1,94 @@
+package agoda;
+
+import java.util.Stack;
+
+public class ReverseSubStringsBetweenEachPairOfParantheses {
+
+    public static void main(String ar[]) {
+
+        ReverseSubStringsBetweenEachPairOfParantheses unit = new ReverseSubStringsBetweenEachPairOfParantheses();
+
+        String s = "s()uteawj((eg))";
+
+        System.out.println("Reversed substring within brackets is " + unit.reverseParentheses(s));
+    }
+
+    public String reverseParentheses(String s) {
+        if (!(s.contains("(") && s.contains(")"))) {
+            return s;
+        } else if (s.indexOf("(") > 0) {
+            int startingIndexOfBracket = s.indexOf("(");
+            String prefixString = s.substring(0, startingIndexOfBracket);
+            return prefixString + parseInputAndPublishFinalResult(s.substring(startingIndexOfBracket, s.length()));
+        } else {
+            return parseInputAndPublishFinalResult(s);
+        }
+    }
+
+    private String parseInputAndPublishFinalResult(String string) {
+        char arr[] = string.toCharArray();
+        int index = 0;
+        Stack<Node> utilityStack = new Stack<>();
+
+        String intermittentString = "";
+        int intermittentStringStartIndex = -1;
+        int intermittentStringEndIndex = -1;
+        boolean flag = false;
+
+        while (index < string.length()) {
+            if (arr[index] == '(') {
+                StringBuilder sb = new StringBuilder();
+                Node node = new Node(index, sb);
+                utilityStack.push(node);
+                flag = true;
+            } else if (arr[index] == ')') {
+            Node poppedNode = utilityStack.pop();
+            flag = false;
+
+            if (intermittentString.isEmpty()) {
+                intermittentStringStartIndex = poppedNode.startIndex;
+                intermittentStringEndIndex = poppedNode.endIndex;
+                intermittentString = poppedNode.sb.reverse().toString();
+            } else {
+                if (poppedNode.endIndex < intermittentStringStartIndex) {
+                    poppedNode.sb.append(intermittentString);
+                    intermittentString = poppedNode.sb.reverse().toString();
+                    intermittentStringStartIndex = poppedNode.startIndex;
+                } else if (poppedNode.startIndex > intermittentStringEndIndex) {
+                    intermittentString += poppedNode.sb.reverse().toString();
+                    intermittentStringEndIndex = poppedNode.endIndex;
+                }
+            }
+            } else {
+                if (flag) {
+                    Node peekedNode = utilityStack.peek();
+                    peekedNode.sb.append(arr[index]);
+                    peekedNode.endIndex = index;
+                } else {
+                    if (!intermittentString.isEmpty()) {
+                        if (index > intermittentStringEndIndex) {
+                            intermittentString += arr[index];
+                            intermittentStringEndIndex = index;
+                        }
+                    } else {
+                        intermittentString += arr[index];
+                        intermittentStringEndIndex = index;
+                    }
+                }
+            }
+            index++;
+        }
+        return intermittentString;
+    }
+
+    static class Node {
+        private int startIndex;
+        private int endIndex;
+        private StringBuilder sb;
+
+        public Node(int startIndex, StringBuilder sb) {
+            this.startIndex = startIndex;
+            this.sb = sb;
+        }
+    }
+}
