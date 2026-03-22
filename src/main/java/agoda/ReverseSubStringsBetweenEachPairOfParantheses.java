c13dd353c2f92ae5ab2af36509a5356171569445
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-22 17:05:06 +0530

    Adding useful information as part of comments git add agoda/ReverseSubStringsBetweenEachPairOfParantheses.java

--- src/main/java/agoda/ReverseSubStringsBetweenEachPairOfParantheses.java
+++ src/main/java/agoda/ReverseSubStringsBetweenEachPairOfParantheses.java
@@ -2,6 +2,22 @@ package agoda;
 
 import java.util.Stack;
 
+/**
+Problem : 1190
+
+You are given a string s that consists of lower case English letters and brackets.
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+
+Constraints:-
+a.) 1 <= s.length <= 2000
+b.) s only contains lower case English characters and parentheses.
+c.) It is guaranteed that all parentheses are balanced.
+ * */
+
 public class ReverseSubStringsBetweenEachPairOfParantheses {
 
     public static void main(String ar[]) {
