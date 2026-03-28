701ec6e197c871eff1a52478182b8b1b5173e9b2
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-28 16:51:54 +0530

    Adding time && space complexity git add capitalOne/greedy/DeliverySystemUsingDrones.java

--- src/main/java/capitalOne/greedy/DeliverySystemUsingDrones.java
+++ src/main/java/capitalOne/greedy/DeliverySystemUsingDrones.java
@@ -5,6 +5,33 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
+/**
+ You are designing a delivery system using drones in a linear warehouse. The warehouse is represented as a number line starting at position 0 and
+ ending at position target (target > 0). Along this line, there are charging stations placed at various positions, represented by an array stations,
+ where stations[i] is the position of the ith charging station.
+ Each drone has a limited battery that allows it to travel a maximum of 10 units after being fully charged. For example, if a drone is charged at position 12,
+ it can travel to positions 12, 13, 14... up to position 22 (inclusive), but cannot reach position 23 or beyond without recharging.
+
+ Your delivery protocol requires the following steps:
+
+ From your current position, pick up the cargo and carry it on foot to the nearest charging station ahead of you.
+ If there are no more stations ahead, carry the cargo on foot to the target position.
+ Deploy a fully charged drone from this station and send it with the cargo as far as possible toward the target.
+ If the target hasn't been reached, walk to the point where the drone landed to retrieve the cargo, then repeat from step 1.
+ Your task is to calculate the total distance over which you must carry the cargo on foot, from position 0 to position target.
+
+ Note: You are not expected to provide the most optimal solution, but a solution with time complexity not worse than O(stations.length x target) will fit within the execution time limit.
+
+ Example:
+ For target = 23 and stations = [7, 4, 14], the output should be solution(target, stations) = 4
+
+ Starting at 0, you find the nearest station at position stations[1] = 4, so you carry the cargo on foot for 4 units to get there, and then deploy a drone that travels to position 14.
+ There is another station at 14 (stations[2] = 14), which you use to deploy another drone that reaches the target position target = 23
+
+ Space Complexity = O(N), where N being number of stations
+ Time Complexity = O(NLogN), where N being number of stations
+ * */
+
 public class DeliverySystemUsingDrones {
 
     public static void main(String ar[]) {
