a33475d2e44fd9a3dd3da8a03d9a6b859cfe28e6
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-03-28 14:45:03 +0530

    Adding time && space complexity for the optimal solution git add capitalOne/dp/NumberOfAdjacentElementsWithSameColor.java

--- src/main/java/capitalOne/dp/NumberOfAdjacentElementsWithSameColor.java
+++ src/main/java/capitalOne/dp/NumberOfAdjacentElementsWithSameColor.java
@@ -2,6 +2,36 @@ package capitalOne.dp;
 
 import java.util.*;
 
+/**
+Problem : 2672
+
+You are given an integer n representing an array colors of length n where all elements are set to 0's meaning uncolored.
+You are also given a 2D integer array queries where queries[i] = [indexi, colori].
+
+For the ith query:
+
+Set colors[indexi] to colori.
+Count the number of adjacent pairs in colors which have the same color (regardless of colori).
+
+Return an array answer of the same length as queries where answer[i] is the answer to the ith query
+
+Constraints:-
+
+a.) 1 <= n <= 10^5
+b.) 1 <= queries.length <= 10^5
+c.) queries[i].length == 2
+d.) 0 <= indexi <= n - 1
+e.) 1 <=  colori <= 10^5
+
+Brute Force :-
+ a.) Time Complexity = O(queries.length * n)
+ b.) Space Complexity = O(n)
+
+ Optimal Approach :-
+ a.) Time Complexity = O(queries.length) [In the below solution could have made used of simple integer array instead of HashMap]
+ b.) Space Complexity = O(n)
+ * */
+
 public class NumberOfAdjacentElementsWithSameColor {
 
     public static void main(String ar[]) {
