5ba0ccd85946a7016b551138f84c9397eceb61a2
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-04-14 19:19:28 +0530

    Pushing accepted solution for the problem git add microsoft/arrays/MergeIntervals.java

---
+++ src/main/java/microsoft/arrays/MergeIntervals.java
@@ -0,0 +1,68 @@
+package microsoft.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MergeIntervals {
+
+    public static void main(String ar[]) {
+        MergeIntervals unit = new MergeIntervals();
+
+       int intervals[][] = {{1,3},{2,6},{8,10},{15,18}};
+       int result[][] = unit.merge(intervals);
+    }
+
+    public int[][] merge(int[][] intervals) {
+
+        List<Interval> intervalList = new ArrayList<>();
+        List<Interval> result = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++) {
+            intervalList.add(new Interval(intervals[i][0], intervals[i][1]));
+        }
+
+        Collections.sort(intervalList, (x, y) -> {
+            return x.startTime < y.startTime ? -1 : x.startTime > y.startTime ? 1 : 0;
+        });
+
+        processToMergeIntervals(intervalList, result);
+
+        int finalResult[][] = new int[result.size()][2];
+
+        for (int i = 0; i < result.size(); i++) {
+            finalResult[i][0] = result.get(i).startTime;
+            finalResult[i][1] = result.get(i).endTime;
+        }
+
+        return finalResult;
+    }
+
+    private void processToMergeIntervals(List<Interval> intervalList, List<Interval> result) {
+        Interval previousInterval = intervalList.get(0);
+
+        for (int i = 1; i < intervalList.size(); i++) {
+            Interval currentInterval = intervalList.get(i);
+            if (currentInterval.startTime <= previousInterval.endTime) {
+                previousInterval = new Interval(previousInterval.startTime, Math.max(previousInterval.endTime, currentInterval.endTime));
+            } else {
+                result.add(previousInterval);
+                previousInterval = currentInterval;
+            }
+        }
+        result.add(previousInterval);
+    }
+
+    static class Interval {
+        private int startTime;
+        private int endTime;
+
+        public Interval(int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+    }
+
+
+
+}
