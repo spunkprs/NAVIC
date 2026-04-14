4084add7c83cfdf6b83c4489da9c737ce1249cb5
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-04-14 17:56:42 +0530

    Adding important information as part of comments git add microsoft/dp/MaximumProfitInJobScheduling.java

--- src/main/java/microsoft/dp/MaximumProfitInJobScheduling.java
+++ src/main/java/microsoft/dp/MaximumProfitInJobScheduling.java
@@ -2,6 +2,28 @@ package microsoft.dp;
 
 import java.util.*;
 
+/**
+Problem : 1235
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X
+
+
+Constraints:-
+
+a.) 1 <= startTime.length == endTime.length == profit.length <= 5 * 10^4
+b.) 1 <= startTime[i] < endTime[i] <= 10^9
+c.) 1 <= profit[i] <= 10^4
+
+23 / 35 testcases passed
+Approach Used : DP
+
+Time Complexity = O(N^2)
+Space Complexity = O(N)
+ * */
+
 public class MaximumProfitInJobScheduling {
 
     private Set<Node> visitedNodes = new HashSet<>();
