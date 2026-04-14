9c69a156b9440fef26621e473d18c543234e9373
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-04-14 20:01:27 +0530

    Pushing accepted solution for the problem git add microsoft/bfs/LargestJumpingNumber.java

---
+++ src/main/java/microsoft/bfs/LargestJumpingNumber.java
@@ -0,0 +1,57 @@
+package microsoft.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LargestJumpingNumber {
+
+    public static void main(String ar[]) {
+        LargestJumpingNumber unit = new LargestJumpingNumber();
+        int number = 50;
+        System.out.println("Largest jumping number less than " + number + " is " + unit.fetchLargestJumpingNumber(number));
+    }
+
+    public int fetchLargestJumpingNumber(int number) {
+
+        if (number <= 10) {
+            return number - 1;
+        } else {
+            return processToFetchLargestJumpingNumber(number);
+        }
+    }
+
+    private int processToFetchLargestJumpingNumber(int number) {
+        int result = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < 10; i++) {
+            queue.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            int peekedNum = queue.peek();
+            String sb = String.valueOf(peekedNum);
+
+            int lastDigit = peekedNum % 10;
+
+            if (lastDigit != 0) {
+                int numOne = Integer.parseInt(sb + (lastDigit + 1));
+                int numTwo = Integer.parseInt(sb + (lastDigit - 1));
+                if (numOne < number) {
+                    queue.add(numOne);
+                }
+                if (numTwo < number) {
+                    queue.add(numTwo);
+                }
+            } else {
+                int numOne = Integer.parseInt(sb + (lastDigit + 1));
+                if (numOne < number) {
+                    queue.add(numOne);
+                }
+            }
+            result = peekedNum > result ? peekedNum : result;
+            queue.poll();
+        }
+        return result;
+    }
+
+}
