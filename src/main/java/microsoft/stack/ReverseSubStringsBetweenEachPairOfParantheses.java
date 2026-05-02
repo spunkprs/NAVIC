58ed9b9dff717778edc38aca773711da0b2b2dea
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-02 16:53:48 +0530

    Accepted solution for the problem git add microsoft/stack/ReverseSubStringsBetweenEachPairOfParantheses.java

---
+++ src/main/java/microsoft/stack/ReverseSubStringsBetweenEachPairOfParantheses.java
@@ -0,0 +1,61 @@
+package microsoft.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class ReverseSubStringsBetweenEachPairOfParantheses {
+
+    public static void main(String ar[]) {
+        ReverseSubStringsBetweenEachPairOfParantheses unit = new ReverseSubStringsBetweenEachPairOfParantheses();
+        String s = "a(bcdefghijkl(mno)p)q";
+        System.out.print("Reversed substring is " + unit.reverseParentheses(s));
+    }
+
+    public String reverseParentheses(String s) {
+        char arr[] = s.toCharArray();
+        Stack<String> stack = new Stack<>();
+        processToReverseString(stack, arr);
+        if (stack.size() == 1) {
+            return stack.pop();
+        } else {
+            StringBuilder sb = new StringBuilder();
+            List<String> intermittentResult = new ArrayList<>();
+
+            while (!stack.isEmpty()) {
+                intermittentResult.add(stack.peek());
+                stack.pop();
+            }
+
+            for (int i = intermittentResult.size() - 1; i >= 0; i--) {
+                sb.append(intermittentResult.get(i));
+            }
+            return sb.toString();
+        }
+    }
+
+    private void processToReverseString(Stack<String> stack, char arr[]) {
+        int index = 0;
+
+        while (index < arr.length) {
+            if (arr[index] == '(' || Character.isLowerCase(arr[index])) {
+                stack.push(String.valueOf(arr[index]));
+            } else if (arr[index] == ')') {
+                StringBuilder sb = new StringBuilder();
+                while (!stack.peek().equals("(")) {
+                    String peekedString = stack.peek();
+                    if (peekedString.length() > 1) {
+                        StringBuilder intermittentSb = new StringBuilder(peekedString);
+                        sb.append(intermittentSb.reverse());
+                    } else {
+                        sb.append(peekedString);
+                    }
+                    stack.pop();
+                }
+                stack.pop();
+                stack.push(sb.toString());
+            }
+            index++;
+        }
+    }
+}
