a27b173ae5e9e2f95cd79fd2511eea5a37104656
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-18 20:10:16 +0530

    Adding description as part of comments git add microsoft/binarytree/MaximumProductOfSplittedBinaryTree.java

--- src/main/java/microsoft/binarytree/MaximumProductOfSplittedBinaryTree.java
+++ src/main/java/microsoft/binarytree/MaximumProductOfSplittedBinaryTree.java
@@ -1,5 +1,21 @@
 package microsoft.binarytree;
 
+/**
+Problem : 1339
+
+Given the root of a binary tree, split the binary tree into two subtrees by removing one edge such that the product of
+the sums of the subtrees is maximized.
+
+Return the maximum product of the sums of the two subtrees. Since the answer may be too large, return it modulo 109 + 7.
+
+Note that you need to maximize the answer before taking the mod and not after taking it.
+
+Constraints:-
+
+a.) The number of nodes in the tree is in the range [2, 5 * 104].
+b.) 1 <= Node.val <= 104
+ * */
+
 public class MaximumProductOfSplittedBinaryTree {
 
     private long maxProduct = 1;
