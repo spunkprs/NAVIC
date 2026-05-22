f2368172aa9d83830094f6e76d6a901b1be87bda
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-22 19:40:36 +0530

    Adding description as part of comments git add microsoft/stack/BuildingsWithOceanView.java

--- src/main/java/microsoft/stack/BuildingsWithOceanView.java
+++ src/main/java/microsoft/stack/BuildingsWithOceanView.java
@@ -2,6 +2,24 @@ package microsoft.stack;
 
 import java.util.Stack;
 
+/**
+Problem : 1762
+
+Link : https://leetcode.com/problems/buildings-with-an-ocean-view/description/?envType=problem-list-v2&envId=stack
+
+There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
+
+The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean without obstructions.
+Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+
+Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+
+
+Constraints:-
+a.) 1 <= heights.length <= pow(10,5)
+b.) 1 <= heights[i] <= pow(10,9)
+ * */
+
 public class BuildingsWithOceanView {
 
     public int[] findBuildings(int[] heights) {
