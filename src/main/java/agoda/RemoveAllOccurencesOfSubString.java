bcb8972b5210dc93d64409520bc14de176d66400
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-24 18:26:53 +0530

    Adding time && space complexity as part of comments git add agoda/RemoveAllOccurencesOfSubString.java

--- src/main/java/agoda/RemoveAllOccurencesOfSubString.java
+++ src/main/java/agoda/RemoveAllOccurencesOfSubString.java
@@ -1,5 +1,26 @@
 package agoda;
 
+/**
+Problem : 1910
+Link : https://leetcode.com/problems/remove-all-occurrences-of-a-substring/description/?envType=problem-list-v2&envId=stack
+
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+
+A substring is a contiguous sequence of characters in a string.
+
+
+Constraints:-
+a.) 1 <= s.length <= 1000
+b.) 1 <= part.length <= 1000
+c.) s and part consists of lowercase English letters.
+
+Time Complexity = O(N^2/M), where N is the length of word && M is the length of contained word
+Space Complexity = O(N)
+ * */
+
 public class RemoveAllOccurencesOfSubString {
 
     public static void main(String ar[]) {
