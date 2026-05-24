fd0a1e2df322a005f1d03d6d64cd8905a13bb103
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-24 17:12:11 +0530

    Adding time && space complexity as part of comments git add microsoft/stack/MinSwapsToMakeStringBalanced.java

--- src/main/java/microsoft/stack/MinSwapsToMakeStringBalanced.java
+++ src/main/java/microsoft/stack/MinSwapsToMakeStringBalanced.java
@@ -4,6 +4,36 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+/**
+Problem : 1963
+Link : https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description/?envType=problem-list-v2&envId=stack
+
+You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+
+A string is called balanced if and only if:
+
+It is the empty string, or
+It can be written as AB, where both A and B are balanced strings, or
+It can be written as [C], where C is a balanced string.
+You may swap the brackets at any two indices any number of times.
+
+Return the minimum number of swaps to make s balanced.
+
+
+Constraints:-
+
+a.) n == s.length
+b.) 2 <= n <= pow(10,6)
+c.) n is even.
+d.) s[i] is either '[' or ']'.
+e.) The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n / 2.
+
+Time Complexity = O(N)
+Space Complexity = O(N)
+
+Level : Medium
+ * */
+
 public class MinSwapsToMakeStringBalanced {
 
     public static void main(String ar[]) {
