d5f58fbdbb72d12f1939a4888e8a1b81613ae679
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-25 16:39:52 +0530

    Accepted description as part of comments git add microsoft/stack/MinimumDeletionsToMakeStringBalanced.java

--- src/main/java/microsoft/stack/MinimumDeletionsToMakeStringBalanced.java
+++ src/main/java/microsoft/stack/MinimumDeletionsToMakeStringBalanced.java
@@ -2,6 +2,22 @@ package microsoft.stack;
 
 import java.util.Stack;
 
+/**
+Problem : 1653
+Link : https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/description/?envType=problem-list-v2&envId=stack
+You are given a string s consisting only of characters 'a' and 'b'.
+
+You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of
+indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+
+Return the minimum number of deletions needed to make s balanced.
+
+Constraints:-
+
+a.) 1 <= s.length <= 10^5
+b.) s[i] is 'a' or 'b'
+ * */
+
 public class MinimumDeletionsToMakeStringBalanced {
 
     public static void main(String ar[]) {
