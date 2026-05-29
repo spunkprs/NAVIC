dc114e502d4471936626aed8582071cf70e6631c
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-05-29 18:16:14 +0530

    Message git status

--- src/main/java/agoda/greedy/MinimumMovesToEqualArrayElements.java
+++ src/main/java/agoda/greedy/MinimumMovesToEqualArrayElements.java
@@ -4,6 +4,23 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
 
+
+/**
+Problem : 462
+Link : https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/description/
+Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+In one move, you can increment or decrement an element of the array by 1.
+
+Test cases are designed so that the answer will fit in a 32-bit integer.
+
+Constraints:-
+
+a.) n == nums.length
+b.) 1 <= nums.length <= 10^5
+c.) -10^9 <= nums[i] <= 10^9
+ * */
+
 public class MinimumMovesToEqualArrayElements {
 
     public static void main(String ar[]) {
