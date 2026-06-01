6f78001fb4cf60ed8f8bf9ee37d3d27c7d8925ad
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-01 16:18:14 +0530

    Adding description as part of comments git add microsoft/dfs/BattleshipsInTheBoard.java

--- src/main/java/microsoft/dfs/BattleshipsInTheBoard.java
+++ src/main/java/microsoft/dfs/BattleshipsInTheBoard.java
@@ -2,6 +2,26 @@ package microsoft.dfs;
 
 import java.util.*;
 
+/**
+Problem : 419
+Link : https://leetcode.com/problems/battleships-in-a-board/description/?envType=problem-list-v2&envId=depth-first-search
+
+Given an m x n matrix board where each cell is a battleship 'X' or empty '.', return the number of the battleships on board.
+
+Battleships can only be placed horizontally or vertically on board. In other words, they can only be made of the shape
+1 x k (1 row, k columns) or k x 1 (k rows, 1 column), where k can be of any size. At least one horizontal or vertical cell
+separates between two battleships (i.e., there are no adjacent battleships).
+
+Constraints:-
+
+a.) m == board.length
+b.) n == board[i].length
+c.) 1 <= m, n <= 200
+d.) board[i][j] is either '.' or 'X'.
+
+Level : Medium
+ * */
+
 public class BattleshipsInTheBoard {
 
     public int countBattleships(char[][] board) {
