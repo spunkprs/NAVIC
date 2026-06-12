562885e97b6dc8e8cb440fea3ed0ceb9d1311849
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-12 18:09:43 +0530

    Adding description as part of comments git add microsoft/twpointer/GetEqualSubStringsWIthInBudget.java

--- src/main/java/microsoft/twpointer/GetEqualSubStringsWIthInBudget.java
+++ src/main/java/microsoft/twpointer/GetEqualSubStringsWIthInBudget.java
@@ -1,5 +1,25 @@
 package microsoft.twpointer;
 
+/**
+Problem : 1208
+Link : https://leetcode.com/problems/get-equal-substrings-within-budget/description/?envType=company&envId=jpmorgan&favoriteSlug=jpmorgan-all
+
+You are given two strings s and t of the same length and an integer maxCost.
+
+You want to change s to t. Changing the ith character of s to ith character of t costs |s[i] - t[i]| (i.e., the absolute difference
+between the ASCII values of the characters).
+
+Return the maximum length of a substring of s that can be changed to be the same as the corresponding substring of t with a cost
+less than or equal to maxCost. If there is no substring from s that can be changed to its corresponding substring from t, return 0.
+
+Constraints:-
+
+a.) 1 <= s.length <= 10^5
+b.) t.length == s.length
+c.) 0 <= maxCost <= 10^6
+d.) s and t consist of only lowercase English letters.
+ * */
+
 public class GetEqualSubStringsWIthInBudget {
 
     public static void main(String ar[]) {
