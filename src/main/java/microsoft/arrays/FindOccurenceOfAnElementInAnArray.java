44a6a80a2736748d449de66c64d18524a5ec8c31
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-21 09:00:42 +0530

    Adding description as part of comments git add microsoft/arrays/FindOccurenceOfAnElementInAnArray.java

--- src/main/java/microsoft/arrays/FindOccurenceOfAnElementInAnArray.java
+++ src/main/java/microsoft/arrays/FindOccurenceOfAnElementInAnArray.java
@@ -3,6 +3,24 @@ package microsoft.arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+Problem : 3159
+Link : https://leetcode.com/problems/find-occurrences-of-an-element-in-an-array/description/?envType=company&envId=jpmorgan&favoriteSlug=jpmorgan-all
+You are given an integer array nums, an integer array queries, and an integer x.
+
+For each queries[i], you need to find the index of the queries[i]th occurrence of x in the nums array. If there are fewer
+than queries[i] occurrences of x, the answer should be -1 for that query.
+
+Return an integer array answer containing the answers to all queries.
+
+
+Constraints:-
+
+a.) 1 <= nums.length, queries.length <= 10^5
+b.) 1 <= queries[i] <= 10^5
+c.) 1 <= nums[i], x <= 10^4
+ * */
+
 public class FindOccurenceOfAnElementInAnArray {
 
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
