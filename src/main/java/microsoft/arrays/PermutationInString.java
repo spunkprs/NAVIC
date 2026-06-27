d66bac35fdc2513400816bb31a8b5c38b5d229e8
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-27 19:53:38 +0530

    Adding description as part of comments git add microsoft/arrays/PermutationInString.java

--- src/main/java/microsoft/arrays/PermutationInString.java
+++ src/main/java/microsoft/arrays/PermutationInString.java
@@ -3,6 +3,21 @@ package microsoft.arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+Problem : 567
+Link : https://leetcode.com/problems/permutation-in-string/description/
+
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+Constraints:-
+
+a.) 1 <= s1.length, s2.length <= 10^4
+b.) s1 and s2 consist of lowercase English letters
+
+ * */
+
 public class PermutationInString {
 
     public static void main(String ar[]) {
