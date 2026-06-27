26a68f9721aca884c82203147535de80a9b59287
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-27 14:49:48 +0530

    Adding time && space complexity as part of comments git add microsoft/binarytree/BinaryTreeLongestConsecutiveSequence.java

--- src/main/java/microsoft/binarytree/BinaryTreeLongestConsecutiveSequence.java
+++ src/main/java/microsoft/binarytree/BinaryTreeLongestConsecutiveSequence.java
@@ -1,5 +1,25 @@
 package microsoft.binarytree;
 
+
+/**
+Problem : 298
+Link : https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/description/
+
+Given the root of a binary tree, return the length of the longest consecutive sequence path.
+
+A consecutive sequence path is a path where the values increase by one along the path.
+
+Note that the path can start at any node in the tree, and you cannot go from a node to its parent in the path.
+
+Constraints:-
+
+a.) The number of nodes in the tree is in the range [1, 3 * 10^4].
+b.) -3 * 10^4 <= Node.val <= 3 * 10^4
+
+Time Complexity = O(N), where N being number of nodes in the tree
+Space Complexity = O(N) [Because of keeping stack calls as I have made use of DFS approach]
+ * */
+
 public class BinaryTreeLongestConsecutiveSequence {
 
     private int result = 0;
