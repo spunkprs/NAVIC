12bef273bfe0e480302cc9b785a3663210c7e08b
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-28 19:15:16 +0530

    Adding description as part of comments git add microsoft/arrays/MinimumWindowSubSequence.java

--- src/main/java/microsoft/arrays/MinimumWindowSubSequence.java
+++ src/main/java/microsoft/arrays/MinimumWindowSubSequence.java
@@ -1,5 +1,23 @@
 package microsoft.arrays;
 
+/**
+Problem : 727
+Link : https://leetcode.com/problems/minimum-window-subsequence/description/
+Level : HARD
+Given strings s1 and s2, return the minimum contiguous substring part of s1, so that s2 is a subsequence of the part.
+
+If there is no such window in s1 that covers all characters in s2, return the empty string "".
+If there are multiple such minimum-length windows, return the one with the left-most starting index.
+
+
+Constraints:-
+
+a.) 1 <= s1.length <= 2 * 10^4
+b.) 1 <= s2.length <= 100
+c.) s1 and s2 consist of lowercase English letters.
+ * */
+
+
 public class MinimumWindowSubSequence {
 
     private int startIndexFinal = -1;
