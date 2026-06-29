b7f3f16a78d2d97c6e5a5c3b6495acc435772dcf
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-29 13:04:16 +0530

    Adding description as part of comments git add microsoft/dfs/DetonateMaximumBombs.java

--- src/main/java/microsoft/dfs/DetonateMaximumBombs.java
+++ src/main/java/microsoft/dfs/DetonateMaximumBombs.java
@@ -2,6 +2,31 @@ package microsoft.dfs;
 
 import java.util.*;
 
+
+/**
+Problem : 2101
+Link : https://leetcode.com/problems/detonate-the-maximum-bombs/description/
+
+You are given a list of bombs. The range of a bomb is defined as the area where its effect can be felt.
+This area is in the shape of a circle with the center as the location of the bomb.
+
+The bombs are represented by a 0-indexed 2D integer array bombs where bombs[i] = [xi, yi, ri].
+xi and yi denote the X-coordinate and Y-coordinate of the location of the ith bomb, whereas ri denotes the radius of its range.
+
+You may choose to detonate a single bomb. When a bomb is detonated, it will detonate all bombs that lie in its range.
+These bombs will further detonate the bombs that lie in their ranges.
+
+Given the list of bombs, return the maximum number of bombs that can be detonated if you are allowed to detonate only one bomb.
+
+Constraints:-
+
+a.) 1 <= bombs.length <= 100
+b.) bombs[i].length == 3
+c.) 1 <= xi, yi, ri <= 10^5
+
+
+ * */
+
 public class DetonateMaximumBombs {
 
     public static void main(String ar[]) {
