02c52154e7cc143cad493c51d9c0da0e28eb6349
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-06-30 17:17:38 +0530

    Adding description as part of comments git add microsoft/dp/CombinationSum4.java

--- src/main/java/microsoft/dp/CombinationSum4.java
+++ src/main/java/microsoft/dp/CombinationSum4.java
@@ -3,6 +3,22 @@ package microsoft.dp;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+Problem : 377
+Link : https://leetcode.com/problems/combination-sum-iv/description/?envType=problem-list-v2&envId=dynamic-programming
+
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+Constraints:
+
+a.) 1 <= nums.length <= 200
+b.) 1 <= nums[i] <= 1000
+c.) All the elements of nums are unique.
+d.) 1 <= target <= 1000
+ * */
+
 public class CombinationSum4 {
 
     public static void main(String ar[]) {
