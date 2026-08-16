26bc518476763f7bbc5e7ac0715bea1c96e2b8cb
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-08-16 19:52:36 +0530

    Pushing partially accepted solution for the problem git add microsoft/arrays/NumebrOfLongestINcreasingSubsequence.java

---
+++ src/main/java/microsoft/arrays/NumebrOfLongestINcreasingSubsequence.java
@@ -0,0 +1,79 @@
+package microsoft.arrays;
+
+public class NumebrOfLongestINcreasingSubsequence {
+
+    private boolean flag = false;
+
+    public static void main(String ar[]) {
+        NumebrOfLongestINcreasingSubsequence unit = new NumebrOfLongestINcreasingSubsequence();
+        int arr[] = {2, 4, 6, 5, 8, 2};
+        //int arr[] = {1,2,4,3,5,4,7,2};
+
+        //int arr[] = {2, 2, 2, 2, 2};
+        System.out.print("Number of LIS is " + unit.findNumberOfLIS(arr));
+    }
+
+    public int findNumberOfLIS(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
+        int helperLengthArr[] = new int[nums.length];
+        int helperCountArr[] = new int[nums.length];
+        int result = 0;
+
+        processToFindNumberOfLISApproachOne(nums, helperLengthArr, helperCountArr);
+        if (!flag) {
+            for (int i = 0; i < helperCountArr.length; i++) {
+                result += helperCountArr[i];
+            }
+        } else {
+            int maxLength = 0;
+            for (int i = 0; i < helperLengthArr.length; i++) {
+                if (helperLengthArr[i] > maxLength) {
+                    maxLength = helperLengthArr[i];
+                }
+            }
+
+            for (int i = 0; i < helperLengthArr.length; i++) {
+                if (helperLengthArr[i] == maxLength) {
+                    result += helperCountArr[i];
+                }
+            }
+        }
+        return result;
+    }
+
+    private void processToFindNumberOfLISApproachOne(int[] nums, int helperLengthArr[], int helperCountArr[]) {
+
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (j == nums.length - 1) {
+                helperLengthArr[j] = 1;
+                helperCountArr[j] = 1;
+            } else {
+                for (int i = j + 1; i < nums.length; i++) {
+                    if (nums[j] < nums[i]) {
+                        flag = true;
+                        int existingLength = helperLengthArr[j];
+                        int existingCount = helperCountArr[j];
+                        if (existingCount == 0) {
+                            helperLengthArr[j] = helperLengthArr[i] + 1;
+                            helperCountArr[j] = helperCountArr[i];
+                        } else {
+                            if (existingLength < helperLengthArr[i] + 1) {
+                                helperLengthArr[j] = helperLengthArr[i] + 1;
+                                helperCountArr[j] = helperCountArr[i];
+                            } else if (existingLength == helperLengthArr[i] + 1) {
+                                helperCountArr[j] = 1 + helperCountArr[j];
+                            }
+                        }
+                    } else {
+                        if (helperLengthArr[j] == 0) {
+                            helperLengthArr[j] = 1;
+                            helperCountArr[j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
