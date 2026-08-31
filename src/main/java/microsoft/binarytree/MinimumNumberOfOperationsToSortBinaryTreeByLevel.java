c51a7b36b4649c141dd8838d3e2f22172baa7f45
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-08-31 16:14:31 +0530

    Adding description as part of comments git add microsoft/binarytree/MinimumNumberOfOperationsToSortBinaryTreeByLevel.java

--- src/main/java/microsoft/binarytree/MinimumNumberOfOperationsToSortBinaryTreeByLevel.java
+++ src/main/java/microsoft/binarytree/MinimumNumberOfOperationsToSortBinaryTreeByLevel.java
@@ -3,6 +3,26 @@ package microsoft.binarytree;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+Problem : 2471
+Link : https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/description/
+
+You are given the root of a binary tree with unique values.
+
+In one operation, you can choose any two nodes at the same level and swap their values.
+
+Return the minimum number of operations needed to make the values at each level sorted in a strictly increasing order.
+
+The level of a node is the number of edges along the path between it and the root node.
+
+
+Constraints:-
+
+a.) The number of nodes in the tree is in the range [1, 10^5].
+b.) 1 <= Node.val <= 10^5
+c.) All the values of the tree are unique.
+ * */
+
 public class MinimumNumberOfOperationsToSortBinaryTreeByLevel {
 
     public static void main(String ar[]) {
