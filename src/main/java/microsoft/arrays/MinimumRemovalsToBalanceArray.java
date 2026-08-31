df8d2c300af12f70fbaeada09e7ef32081233b1d
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-08-31 19:09:48 +0530

    Adding time && space complexity as part of comments git add microsoft/arrays/MinimumRemovalsToBalanceArray.java

--- src/main/java/microsoft/arrays/MinimumRemovalsToBalanceArray.java
+++ src/main/java/microsoft/arrays/MinimumRemovalsToBalanceArray.java
@@ -2,6 +2,31 @@ package microsoft.arrays;
 
 import java.util.Arrays;
 
+/**
+Problem : 3634
+Link : https://leetcode.com/problems/minimum-removals-to-balance-array/description/?envType=company&envId=salesforce&favoriteSlug=salesforce-six-months
+
+You are given an integer array nums and an integer k.
+
+An array is considered balanced if the value of its maximum element is at most k times the minimum element.
+
+You may remove any number of elements from nums without making it empty.
+
+Return the minimum number of elements to remove so that the remaining array is balanced.
+
+Note: An array of size 1 is considered balanced as its maximum and minimum are equal, and the condition always holds true.
+
+
+Constraints:-
+
+a.) 1 <= nums.length <= 10^5
+b.) 1 <= nums[i] <= 10^9
+c.) 1 <= k <= 10^5
+
+Time Complexity = O(n * log(n))
+Space Complexity = O(1), but memory used for sorting would be O(N) in the worst case
+ * */
+
 public class MinimumRemovalsToBalanceArray {
 
     public static void main(String ar[]) {
