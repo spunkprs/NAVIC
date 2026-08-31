44735aa233ed3c6882dbfbceb46f3d744cedf176
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-08-31 08:45:01 +0530

    added imperative comments git add lld/notificationSystem/factory/MessageCreationFactory.java

--- src/main/java/lld/notificationSystem/factory/MessageCreationFactory.java
+++ src/main/java/lld/notificationSystem/factory/MessageCreationFactory.java
@@ -4,6 +4,13 @@ import lld.notificationSystem.channel.Channels;
 import lld.notificationSystem.model.Message;
 import java.util.Map;
 
+/**
+Though we are making use of factory but to get rid of if-else block
+we are making use of Strategy i.e MessageCreator [different implementations available]
+and depending on the channel at the run time we wil decide which implementation of MessageCreator
+will be used
+ * */
+
 public class MessageCreationFactory {
 
     private Map<Channels, MessageCreator> messageCreatorMap;
