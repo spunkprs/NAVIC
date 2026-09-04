b279f376384667657ba43d08d20c744058b02067
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-09-04 10:08:25 +0530

    Adding meaningful comments git status

--- src/main/java/lld/jobScheduler/dispatcher/DispatcherProcess.java
+++ src/main/java/lld/jobScheduler/dispatcher/DispatcherProcess.java
@@ -5,6 +5,18 @@ import lld.jobScheduler.model.JobRun;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.ExecutorService;
 
+/**
+This process is responsible for pulling task out of DelayedQueue && pushing it to ExecutorService where individual worker threads
+will take care of running submitted tasks
+
+Have made this dispatcher process light weight i.e could have multiple dispatcher process to poll data from DelayedQueue && get the polled
+task run in it's thread but with that approach chances of a task getting delayed is pretty high because in case few payload execution takes time
+in that case take() method call will be delayed hence task delay will be introduced
+
+To avoid it responsibility of running individual task/payload is given to ExecutorService instead [chances of task/payload getting rejected is
+definitely there in case tasks getting pushed to the ES is way more than worker threads spun by ES]
+ * */
+
 public class DispatcherProcess {
 
     private String processId;
--- src/main/java/lld/jobScheduler/model/Payload.java
+++ src/main/java/lld/jobScheduler/model/Payload.java
@@ -1,5 +1,10 @@
 package lld.jobScheduler.model;
 
+/**
+Payload that will be executed by the worker threads, could have made it non runnable too but since
+I am pushing it to ExecutorService hence made it a Runnable
+ * */
+
 public class Payload implements Runnable {
 
     private String attachedPayload;
