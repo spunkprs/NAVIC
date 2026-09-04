5bfa3251e4e0d2ce0f88c7b03af86fe7231baae4
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-09-04 10:00:36 +0530

    Adding imperative comments git add lld/jobScheduler/model/JobRun.java

--- src/main/java/lld/jobScheduler/model/JobRun.java
+++ src/main/java/lld/jobScheduler/model/JobRun.java
@@ -3,6 +3,11 @@ package lld.jobScheduler.model;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
+/**
+Responsible for holding the payload && time at which payload needs to be executed
+It has to implement interface Delayed because this is something which we will be pushing to DelayedQueue
+ * */
+
 public class JobRun implements Delayed {
 
     private String jobRunId;
@@ -15,11 +20,22 @@ public class JobRun implements Delayed {
         this.attachedPayload = attachedPayload;
     }
 
+    /***
+     This method is used to fetch the delay i.e
+     a.) > 0 [Task not ready to be picked]
+     b.) <= 0 [Task ready to be picked]
+     This method is called by the DelayedQueue when caller makes call to take() method of DelayedQueue --> which means poll object from DelayedQueue
+     */
+
     @Override
     public long getDelay(TimeUnit unit) {
         return unit.convert(this.getExecutionTimeInMillis() - System.currentTimeMillis(),
                 TimeUnit.MILLISECONDS);
     }
+
+    /***
+    This method is used to sort the Delayed tasks[on the similar lines of PriorityQueue]
+     */
 
     @Override
     public int compareTo(Delayed other) {
