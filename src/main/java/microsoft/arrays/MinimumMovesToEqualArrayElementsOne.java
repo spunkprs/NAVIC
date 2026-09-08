ed52dd5c28ba71ccbd44b9adbe2af31168b378db
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-09-08 09:49:28 +0530

    Adding description as part of comments git add microsoft/arrays/MinimumMovesToEqualArrayElementsOne.java

--- src/main/java/microsoft/arrays/MinimumMovesToEqualArrayElementsOne.java
+++ src/main/java/microsoft/arrays/MinimumMovesToEqualArrayElementsOne.java
@@ -2,6 +2,23 @@ package microsoft.arrays;
 
 import java.util.Arrays;
 
+/**
+Problem : 453
+Link : https://leetcode.com/problems/minimum-moves-to-equal-array-elements/description/
+Level : Medium
+
+Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+In one move, you can increment n - 1 elements of the array by 1.
+
+Constraints:-
+
+a.) n == nums.length
+b.) 1 <= nums.length <= 10^5
+c.) -10^9 <= nums[i] <= 10^9
+d.) The answer is guaranteed to fit in a 32-bit integer.
+ * */
+
 public class MinimumMovesToEqualArrayElementsOne {
 
     public static void main(String ar[]) {
