a953fe15b5fb9a4ca49a3c377b5437b34abd6eee
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-09-13 14:51:14 +0530

    Adding description as part of comments git add microsoft/arrays/BrightestPositionOnStreet.java

--- src/main/java/microsoft/arrays/BrightestPositionOnStreet.java
+++ src/main/java/microsoft/arrays/BrightestPositionOnStreet.java
@@ -4,6 +4,30 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+Problem : 2021
+Level : Medium
+Link : https://leetcode.com/problems/brightest-position-on-street/description/
+
+A perfectly straight street is represented by a number line. The street has street lamp(s) on it
+and is represented by a 2D integer array lights. Each lights[i] = [positioni, rangei] indicates
+that there is a street lamp at position positioni that lights up the area
+from [positioni - rangei, positioni + rangei] (inclusive).
+
+The brightness of a position p is defined as the number of street lamp that light up the position p.
+
+Given lights, return the brightest position on the street. If there are multiple brightest positions,
+return the smallest one.
+
+Constraints:-
+
+a.) 1 <= lights.length <= 10^5
+b.) lights[i].length == 2
+c.) -10^8 <= positioni <= 10^8
+d.) 0 <= rangei <= 10^8
+
+ * */
+
 public class BrightestPositionOnStreet {
 
     public static void main(String ar[]) {
