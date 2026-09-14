1087afad743cf3abc970ca055fd76939c37d91ea
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-09-14 13:49:20 +0530

    Adding description as part of comments git add microsoft/arrays/MinimumSwapsToGroupAllOnesTogether2.java

--- src/main/java/microsoft/arrays/MinimumSwapsToGroupAllOnesTogether2.java
+++ src/main/java/microsoft/arrays/MinimumSwapsToGroupAllOnesTogether2.java
@@ -2,6 +2,21 @@ package microsoft.arrays;
 
 import java.util.Arrays;
 
+/**
+A swap is defined as taking two distinct positions in an array and swapping the values in them.
+
+A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+
+Given a binary circular array nums, return the minimum number of swaps required to group all 1's
+present in the array together at any location.
+
+Constraints:-
+
+a.) 1 <= nums.length <= 10^5
+b.) nums[i] is either 0 or 1.
+
+ * */
+
 public class MinimumSwapsToGroupAllOnesTogether2 {
 
     public static void main(String ar[]) {
