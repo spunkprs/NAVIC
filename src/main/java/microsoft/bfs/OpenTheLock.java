f953a32bdc0aff64bac5f5883fe5b5fc22348ca7
Prateek Raj Singh <prateekraj1988@gmail.com>
2026-09-20 19:38:27 +0530

    Adding description as part of comments git add microsoft/bfs/OpenTheLock.java

--- src/main/java/microsoft/bfs/OpenTheLock.java
+++ src/main/java/microsoft/bfs/OpenTheLock.java
@@ -2,6 +2,32 @@ package microsoft.bfs;
 
 import java.util.*;
 
+/**
+Problem : 752
+Level : Medium
+Link : https://leetcode.com/problems/open-the-lock/description/?envType=company&envId=coupang&favoriteSlug=coupang-more-than-six-months
+
+
+You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'.
+The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+
+The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+You are given a list of deadends dead ends, meaning if the lock displays any of these codes,
+the wheels of the lock will stop turning and you will be unable to open it.
+
+Given a target representing the value of the wheels that will unlock the lock, return the minimum total number
+of turns required to open the lock, or -1 if it is impossible.
+
+Constraints:-
+
+a.) 1 <= deadends.length <= 500
+b.) deadends[i].length == 4
+c.) target.length == 4
+d.) target will not be in the list deadends.
+e.) target and deadends[i] consist of digits only.
+ * */
+
 public class OpenTheLock {
 
 
